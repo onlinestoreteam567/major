5bcec3f5f33e7456300a224a5e0c625952eb662b
Dmytro Lymar <dmytrolymarv@gmail.com>
2024-09-16 09:19:16 +0300

    Added SearchInput for products searching

---
+++ client/src/components/UI/SearchInput/SearchInput.jsx
@@ -0,0 +1,58 @@
+import { useMemo, useState } from "react";
+import styles from "./index.module.scss";
+
+// Array of products example
+const products = [
+  { id: 1, name: "Laptop" },
+  { id: 2, name: "Smartphone" },
+  { id: 3, name: "Headphones" },
+  { id: 4, name: "Desk Chair" },
+  { id: 5, name: "Coffee Mug" },
+  { id: 6, name: "Notebook" },
+  { id: 7, name: "Gaming Mouse" },
+  { id: 8, name: "Keyboard" },
+  { id: 9, name: "Backpack" },
+  { id: 10, name: "Water Bottle" },
+];
+
+function SearchInput() {
+  const [inputValue, setInputValue] = useState("");
+  const handleInput = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const filteredProducts = useMemo(
+    () =>
+      products.filter((product) => {
+        if (inputValue !== "") {
+          return product.name.toLowerCase().includes(inputValue.toLowerCase());
+        }
+      }),
+    [inputValue]
+  );
+
+  return (
+    <>
+      <input
+        className={styles.searchInput}
+        type="text"
+        onChange={handleInput}
+        value={inputValue}
+      />
+
+      <ul>
+        {inputValue && filteredProducts.length > 0 ? (
+          <ul>
+            {filteredProducts.map((product) => (
+              <li key={product.id}>{product.name}</li>
+            ))}
+          </ul>
+        ) : (
+          inputValue && <p>No products found</p>
+        )}
+      </ul>
+    </>
+  );
+}
+
+export default SearchInput;
