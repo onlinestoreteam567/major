fe4973f30905274281c44c97735c9e9b670a7749
FreshLemur <157699166+FreshLemur@users.noreply.github.com>
2024-09-30 11:56:19 +0300

    Implemented search bar design with updated styles

--- client/src/components/UI/BestSellersBanner/BestSellerCard.jsx
+++ client/src/components/UI/BestSellersBanner/BestSellerCard.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import cl from "./index.module.scss";
 import stars from "../../../assets/svg/banners/stars.svg";
-import hryvniaSymbol from "../../../assets/svg/banners/hryvnia.svg";
+import hryvniaSymbol from "/src/assets/svg/hryvnia.svg";
 
 //* cardData example:
 //* cardData={{
--- client/src/components/UI/SearchInput/SearchInput.jsx
+++ client/src/components/UI/SearchInput/SearchInput.jsx
@@ -1,24 +1,52 @@
 import { useMemo, useState } from "react";
 import PropTypes from "prop-types";
 import cl from "./index.module.scss";
-
-// Some change
+import hryvniaSymbol from "/src/assets/svg/hryvnia.svg";
 
 // Array of products example
 const products = [
-  { id: 1, name: "Laptop" },
-  { id: 2, name: "Smartphone" },
-  { id: 3, name: "Headphones" },
-  { id: 4, name: "Desk Chair" },
-  { id: 5, name: "Coffee Mug" },
-  { id: 6, name: "Notebook" },
-  { id: 7, name: "Gaming Mouse" },
-  { id: 8, name: "Keyboard" },
-  { id: 9, name: "Backpack" },
-  { id: 10, name: "Water Bottle" },
+  {
+    id: 1,
+    name: "Флюїд-шовк для тонкого волосся",
+    urlImg: "/src/assets/png/searchInput/1.png",
+    price: "299",
+  },
+
+  {
+    id: 2,
+    name: "Крем-сироватка для тонкого волосся",
+    urlImg: "/src/assets/png/searchInput/2.png",
+    price: "299",
+  },
+  {
+    id: 3,
+    name: "Крем-сироватка для тонкого волосся",
+    urlImg: "/src/assets/png/searchInput/2.png",
+    price: "299",
+  },
+  {
+    id: 4,
+    name: "Крем-сироватка для тонкого волосся",
+    urlImg: "/src/assets/png/searchInput/2.png",
+    price: "299",
+  },
+  {
+    id: 5,
+    name: "Крем-сироватка для тонкого волосся",
+    urlImg: "/src/assets/png/searchInput/2.png",
+    price: "299",
+  },
+  {
+    id: 6,
+    name: "Крем-сироватка для тонкого волосся",
+    urlImg: "/src/assets/png/searchInput/2.png",
+    price: "299",
+  },
 ];
 
 function SearchInput({ setIsShowInput }) {
+  //TODO: remove search by Space keyboard button
+
   const [inputValue, setInputValue] = useState("");
 
   const handleCloseInput = () => {
@@ -40,9 +68,9 @@ function SearchInput({ setIsShowInput }) {
   );
 
   return (
-    <>
+    <search className={inputValue && cl.activeSearch}>
       <input
-        className={cl.searchInput}
+        className={`${cl.searchInput} ${inputValue && cl.activeSearchInput}`}
         type="text"
         placeholder="Я шукаю..."
         onChange={handleInput}
@@ -51,15 +79,32 @@ function SearchInput({ setIsShowInput }) {
       />
 
       {inputValue && filteredProducts.length > 0 ? (
-        <ul>
-          {filteredProducts.map((product) => (
-            <li key={product.id}>{product.name}</li>
-          ))}
-        </ul>
+        <section className={cl.searchResultsSection}>
+          <hr />
+          <ul>
+            {filteredProducts.map((product) => (
+              <li key={product.id} className={cl.searchResultItem}>
+                <img src={product.urlImg} alt={product.name} />
+                <section className={cl.searchResultInfo}>
+                  <p>{product.name}</p>
+                  <br />
+                  <p className={cl.searchResultPrice}>
+                    {product.price}{" "}
+                    <img
+                      src={hryvniaSymbol}
+                      alt="Hryvnia symbol"
+                      className={cl.hryvniaSymbol}
+                    />
+                  </p>
+                </section>
+              </li>
+            ))}
+          </ul>
+        </section>
       ) : (
         inputValue && <p>No products found</p>
       )}
-    </>
+    </search>
   );
 }
 
