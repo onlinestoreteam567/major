66e0f1cb200c31f091611ad1026b3e523eeb67d2
FreshLemur <157699166+FreshLemur@users.noreply.github.com>
2024-10-02 16:22:20 +0300

    Implement Major info in Main page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import BestSellersBanner from "../components/UI/BestSellersBanner/BestSellersBanner";
 import Catalog from "../components/UI/Catalog/Catalog";
 import MainBanner from "../components/UI/MainBanner/MainBanner";
+import MajorInfo from "../components/UI/MajorInfo/MajorInfo";
 const HomePage = () => {
   return (
     <div>
       <MainBanner />
       <BestSellersBanner />
       <Catalog />
+      <MajorInfo />
     </div>
   );
 };
