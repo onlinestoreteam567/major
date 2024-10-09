8246fae37d4ff02471eb518d67cd7bb8ca72f1e5
liubomyr <liubomyrpoland@gmail.com>
2024-10-09 14:44:31 +0200

    Disabled propTypes eslint linting

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -31,5 +31,6 @@ export default [
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
+    'react/prop-types': ['disabled'],
   },
 ];
