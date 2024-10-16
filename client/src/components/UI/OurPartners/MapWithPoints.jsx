a4be80f99b8bc705512f2c282cf6a56b4670fcc0
FreshLemur <157699166+FreshLemur@users.noreply.github.com>
2024-10-16 11:12:13 +0300

    Refactored OurPartners component for drag functionality

---
+++ client/src/components/UI/OurPartners/MapWithPoints.jsx
@@ -0,0 +1,87 @@
+import React, { useState, useRef, useEffect } from 'react';
+import cl from './index.module.scss';
+import map from '../../../assets/png/ourPartners/map.png';
+import point from '../../../assets/svg/ourPartners/point.svg';
+
+const MapWithPoints = ({ partnerData, onPointClick }) => {
+  const [dragState, setDragState] = useState({
+    isDragging: false,
+    startX: 0,
+    startY: 0,
+    scrollLeft: 0,
+    scrollTop: 0,
+  });
+
+  const imageContainerRef = useRef(null);
+
+  const handleMouseDown = (e) => {
+    if (imageContainerRef.current) {
+      setDragState({
+        isDragging: true,
+        startX: e.pageX - imageContainerRef.current.offsetLeft,
+        startY: e.pageY - imageContainerRef.current.offsetTop,
+        scrollLeft: imageContainerRef.current.scrollLeft,
+        scrollTop: imageContainerRef.current.scrollTop,
+      });
+    }
+  };
+
+  const handleMouseLeave = () => {
+    setDragState((prevState) => ({ ...prevState, isDragging: false }));
+  };
+
+  const handleMouseUp = () => {
+    setDragState((prevState) => ({ ...prevState, isDragging: false }));
+  };
+
+  const handleMouseMove = (e) => {
+    if (!dragState.isDragging || !imageContainerRef.current) return;
+    e.preventDefault();
+    const x = e.pageX - imageContainerRef.current.offsetLeft;
+    const y = e.pageY - imageContainerRef.current.offsetTop;
+    // Change number for change speed
+    const walkX = (x - dragState.startX) * 1;
+    const walkY = (y - dragState.startY) * 1;
+    imageContainerRef.current.scrollLeft = dragState.scrollLeft - walkX;
+    imageContainerRef.current.scrollTop = dragState.scrollTop - walkY;
+  };
+
+  // UseEffect for the initial centering of the map
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      if (imageContainerRef.current) {
+        const container = imageContainerRef.current;
+        const scrollToY = container.scrollHeight / 2.47 - container.clientHeight / 2;
+        container.scrollTop = scrollToY;
+        const scrollToX = container.scrollWidth / 2 - container.clientWidth / 2;
+        container.scrollLeft = scrollToX;
+      }
+    }, 500);
+    return () => clearTimeout(timeoutId);
+  }, []);
+
+  return (
+    <figure
+      ref={imageContainerRef}
+      onMouseDown={handleMouseDown}
+      onMouseLeave={handleMouseLeave}
+      onMouseUp={handleMouseUp}
+      onMouseMove={handleMouseMove}
+      className={cl.mapContainer}
+    >
+      <img src={map} alt="Map" onDragStart={(e) => e.preventDefault()} />
+      {partnerData.map((partner) => (
+        <img
+          key={partner.className}
+          className={`${cl.point} ${cl[partner.className]}`}
+          src={point}
+          alt={partner.title}
+          onClick={() => onPointClick(partner.className)}
+          onDragStart={(e) => e.preventDefault()}
+        />
+      ))}
+    </figure>
+  );
+};
+
+export default MapWithPoints;
--- client/src/components/UI/OurPartners/OurPartners.jsx
+++ client/src/components/UI/OurPartners/OurPartners.jsx
@@ -1,12 +1,10 @@
+import { useState } from 'react';
 import cl from './index.module.scss';
-import map from '../../../assets/png/ourPartners/map.png';
-import point from '../../../assets/svg/ourPartners/point.svg';
-import { useState, useEffect, useRef } from 'react';
+import MapWithPoints from './MapWithPoints';
 import InformationAboutPartner from './InformationAboutPartner';
 import partnerData from './partnerData';
 
 const OurPartners = () => {
-  // TODO Create responsive design
   const [partnerInteractionState, setPartnerInteractionState] = useState({
     showInformationAboutPartner: false,
     closeAnimation: true,
@@ -22,29 +20,12 @@ const OurPartners = () => {
     googleMapAdress: '',
   });
 
-  const imageContainerRef = useRef(null);
-
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      if (imageContainerRef.current) {
-        const container = imageContainerRef.current;
-        const scrollToY = container.scrollHeight / 2.47 - container.clientHeight / 2;
-        container.scrollTop = scrollToY;
-        const scrollToX = container.scrollWidth / 2 - container.clientWidth / 2;
-        container.scrollLeft = scrollToX;
-      }
-    }, 500);
-
-    return () => clearTimeout(timeoutId);
-  }, []);
-
   const handlePointClick = (className) => {
-    if (partnerInteractionState.clickLocked) return; // Prevent multiple clicks
+    if (partnerInteractionState.clickLocked) return;
     setPartnerInteractionState((prevState) => ({ ...prevState, clickLocked: true }));
     const partner = partnerData.find((p) => p.className === className);
 
     if (partner) {
-      clearTimeout();
       if (className === partnerInteractionState.activePartner) {
         setPartnerInteractionState((prevState) => ({ ...prevState, closeAnimation: true }));
         setTimeout(() => {
@@ -56,9 +37,16 @@ const OurPartners = () => {
           }));
         }, 500);
       } else {
-        setPartnerInteractionState((prevState) => ({ ...prevState, closeAnimation: false, activePartner: className }));
+        setPartnerInteractionState((prevState) => ({
+          ...prevState,
+          closeAnimation: false,
+          activePartner: className,
+        }));
         setInformationAboutPartner(partner);
-        setPartnerInteractionState((prevState) => ({ ...prevState, showInformationAboutPartner: true }));
+        setPartnerInteractionState((prevState) => ({
+          ...prevState,
+          showInformationAboutPartner: true,
+        }));
         setTimeout(() => {
           setPartnerInteractionState((prevState) => ({ ...prevState, clickLocked: false }));
         }, 500);
@@ -69,19 +57,7 @@ const OurPartners = () => {
   return (
     <section className={`${cl.ourPartnersWrapper} ${partnerInteractionState.closeAnimation ? cl.closeAnimation : ''}`}>
       <h2>Наші партнери</h2>
-      <figure ref={imageContainerRef}>
-        <img src={map} alt="" />
-
-        {partnerData.map((partner) => (
-          <img
-            key={partner.className}
-            className={`${cl.point} ${cl[partner.className]}`}
-            src={point}
-            alt={partner.title}
-            onClick={() => handlePointClick(partner.className)}
-          />
-        ))}
-      </figure>
+      <MapWithPoints partnerData={partnerData} onPointClick={handlePointClick} />
       {partnerInteractionState.showInformationAboutPartner && (
         <InformationAboutPartner
           setPartnerInteractionState={setPartnerInteractionState}
