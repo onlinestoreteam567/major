ecce5637d9a2a662e3df82c56bb660b9636edee0
liubomyr <liubomyrpoland@gmail.com>
2024-10-17 11:59:25 +0200

    Add ErrorBoundary

---
+++ client/src/router/error/ErrorBoundary.jsx
@@ -0,0 +1,21 @@
+import { useRouteError, isRouteErrorResponse } from 'react-router-dom';
+
+const ErrorBoundary = () => {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div>
+        <h1>{error.status} Error</h1>
+        <p>{error.statusText}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Unexpected Error</h1>
+      <p>{error?.message || 'An unexpected error occurred.'}</p>
+    </div>
+  );
+};
