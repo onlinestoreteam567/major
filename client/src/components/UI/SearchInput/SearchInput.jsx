76a1daa1037919f38c19c958395c814e6ac29efd
FreshLemur <157699166+FreshLemur@users.noreply.github.com>
2024-10-17 09:02:18 +0300

    Refactored SearchInput for new design

--- client/src/components/UI/SearchInput/SearchInput.jsx
+++ client/src/components/UI/SearchInput/SearchInput.jsx
@@ -1,128 +1,12 @@
-import { useMemo } from 'react';
+import { useMemo, useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import cl from './index.module.scss';
 import hryvniaSymbol from '../../../assets/svg/hryvnia.svg';
-
-// Array of products example
-const products = [
-  {
-    id: 1,
-    name: 'Флюїд-шовк для тонкого волосся',
-    urlImg: '/src/assets/png/searchInput/1.png',
-    price: '299',
-  },
-
-  {
-    id: 2,
-    name: 'Крем-сироватка для тонкого волосся',
-    urlImg: '/src/assets/png/searchInput/2.png',
-    price: '299',
-  },
-  {
-    id: 3,
-    name: 'Крем-сироватка для тонкого волосся',
-    urlImg: '/src/assets/png/searchInput/2.png',
-    price: '299',
-  },
-  {
-    id: 4,
-    name: 'Крем-сироватка для тонкого волосся',
-    urlImg: '/src/assets/png/searchInput/2.png',
-    price: '299',
-  },
-  {
-    id: 5,
-    name: 'Крем-сироватка для тонкого волосся',
-    urlImg: '/src/assets/png/searchInput/2.png',
-    price: '299',
-  },
-  {
-    id: 6,
-    name: 'Крем-сироватка для тонкого волосся',
-    urlImg: '/src/assets/png/searchInput/2.png',
-    price: '299',
-  },
-  {
-    id: 6,
-    name: 'Крем-сироватка для тонкого волосся',
-    urlImg: '/src/assets/png/searchInput/2.png',
-    price: '299',
-  },
-  {
-    id: 6,
-    name: 'Крем-сироватка для тонкого волосся',
-    urlImg: '/src/assets/png/searchInput/2.png',
-    price: '299',
-  },
-  {
-    id: 6,
-    name: 'Крем-сироватка для тонкого волосся',
-    urlImg: '/src/assets/png/searchInput/2.png',
-    price: '299',
-  },
-  {
-    id: 6,
-    name: 'Крем-сироватка для тонкого волосся',
-    urlImg: '/src/assets/png/searchInput/2.png',
-    price: '299',
-  },
-  {
-    id: 6,
-    name: 'Крем-сироватка для тонкого волосся',
-    urlImg: '/src/assets/png/searchInput/2.png',
-    price: '299',
-  },
-  {
-    id: 6,
-    name: 'Крем-сироватка для тонкого волосся',
-    urlImg: '/src/assets/png/searchInput/2.png',
-    price: '299',
-  },
-  {
-    id: 6,
-    name: 'Крем-сироватка для тонкого волосся',
-    urlImg: '/src/assets/png/searchInput/2.png',
-    price: '299',
-  },
-  {
-    id: 6,
-    name: 'Крем-сироватка для тонкого волосся',
-    urlImg: '/src/assets/png/searchInput/2.png',
-    price: '299',
-  },
-  {
-    id: 6,
-    name: 'Крем-сироватка для тонкого волосся',
-    urlImg: '/src/assets/png/searchInput/2.png',
-    price: '299',
-  },
-  {
-    id: 6,
-    name: 'Крем-сироватка для тонкого волосся',
-    urlImg: '/src/assets/png/searchInput/2.png',
-    price: '299',
-  },
-  {
-    id: 6,
-    name: 'Крем-сироватка для тонкого волосся',
-    urlImg: '/src/assets/png/searchInput/2.png',
-    price: '299',
-  },
-  {
-    id: 6,
-    name: 'Крем-сироватка для тонкого волосся',
-    urlImg: '/src/assets/png/searchInput/2.png',
-    price: '299',
-  },
-  {
-    id: 6,
-    name: 'Крем-сироватка для тонкого волосся',
-    urlImg: '/src/assets/png/searchInput/2.png',
-    price: '299',
-  },
-];
+import products from './productsExample';
 
 function SearchInput({ setIsShowInput, inputValue, setInputValue }) {
+  const [isInputFocus, setIsInputFocus] = useState(false);
+
   const handleCloseInput = () => {
     clearTimeout();
     setTimeout(() => {
@@ -132,6 +16,10 @@ function SearchInput({ setIsShowInput, inputValue, setInputValue }) {
 
   const handleInput = (e) => {
     setInputValue(e.target.value);
+  };
+
+  const handleInputFocus = () => {
+    setIsInputFocus(true);
   };
 
   const filteredProducts = useMemo(
@@ -145,40 +33,53 @@ function SearchInput({ setIsShowInput, inputValue, setInputValue }) {
   );
 
   return (
-    <search className={inputValue && cl.activeSearch} onMouseLeave={handleCloseInput}>
-      <input
-        className={`${cl.searchInput} ${inputValue && cl.activeSearchInput}`}
-        type="text"
-        placeholder="Я шукаю..."
-        onChange={handleInput}
-        value={inputValue}
-      />
+    <search onMouseLeave={handleCloseInput}>
+      <div className={inputValue && cl.activeSearch}>
+        <input
+          className={`${cl.searchInput} ${inputValue && cl.activeSearchInput} ${isInputFocus && cl.inputFocus}`}
+          type="text"
+          placeholder="Я шукаю..."
+          onChange={handleInput}
+          value={inputValue}
+          onFocus={handleInputFocus}
+        />
 
-      {inputValue && filteredProducts.length > 0 ? (
-        <section className={cl.searchResultsSection}>
-          <hr />
-          <ul>
-            {filteredProducts.map((product) => (
-              <li key={product.id} className={cl.searchResultItem}>
-                <img src={product.urlImg} alt={product.name} />
-                <section className={cl.searchResultInfo}>
-                  <p>{product.name}</p>
-                  <br />
-                  <p className={cl.searchResultPrice}>
-                    {product.price} <img src={hryvniaSymbol} alt="Hryvnia symbol" className={cl.hryvniaSymbol} />
-                  </p>
-                </section>
-              </li>
-            ))}
-          </ul>
-        </section>
-      ) : (
-        inputValue && (
-          <section className={cl.searchNotFound}>
-            <p>Товарів не знайдено</p>
+        {inputValue && filteredProducts.length > 0 ? (
+          <section className={cl.searchResultsSection}>
+            <hr />
+            <ul>
+              {filteredProducts.slice(0, 3).map((product) => (
+                <li key={product.id} className={cl.searchResultItem}>
+                  <a href=" ">
+                    <img src={product.urlImg} alt={product.name} />
+                  </a>
+                  <section className={cl.searchResultInfo}>
+                    <a href="">
+                      <p className={cl.productName}>{product.name}</p>
+                    </a>
+                    <br />
+                    <p className={cl.productPrice}>
+                      {product.price} <img src={hryvniaSymbol} alt="Hryvnia symbol" className={cl.hryvniaSymbol} />
+                    </p>
+                  </section>
+                </li>
+              ))}
+
+              {filteredProducts.length >= 3 && (
+                <a className={cl.showAll} href=" ">
+                  Показати всі результати пошуку
+                </a>
+              )}
+            </ul>
           </section>
-        )
-      )}
+        ) : (
+          inputValue && (
+            <section className={cl.searchNotFound}>
+              <p>Товарів не знайдено</p>
+            </section>
+          )
+        )}
+      </div>
     </search>
   );
 }
