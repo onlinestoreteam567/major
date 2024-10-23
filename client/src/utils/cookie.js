3f276fe8c84b22da572cee2bf0aa822411373972
liubomyr <liubomyrpoland@gmail.com>
2024-10-23 14:43:53 +0200

    Replaced methods

--- client/src/utils/cookie.js
+++ client/src/utils/cookie.js
@@ -1,18 +1,6 @@
 import Cookies from 'js-cookie';
 
 const cookieService = {
-  allowCookie: (options = {}) => {
-    Cookies.set('isCookieAllowed', 'true', { path: '/', expires: 365, ...options });
-  },
-
-  disallowCookie: (options = {}) => {
-    Cookies.remove('isCookieAllowed', { path: '/', ...options });
-  },
-
-  getIsCookieAllowed: () => {
-    return Cookies.get('isCookieAllowed') === 'true' ? true : false;
-  },
-
   setCookie: function (name, value, options = {}) {
     if (this.getIsCookieAllowed()) {
       Cookies.set(name, value, { path: '/', ...options });
@@ -27,6 +15,18 @@ const cookieService = {
 
   removeCookie: (name, options = {}) => {
     Cookies.remove(name, { path: '/', ...options });
+  },
+
+  allowCookie: (options = {}) => {
+    Cookies.set('isCookieAllowed', 'true', { path: '/', expires: 365, ...options });
+  },
+
+  disallowCookie: (options = {}) => {
+    Cookies.remove('isCookieAllowed', { path: '/', ...options });
+  },
+
+  getIsCookieAllowed: () => {
+    return Cookies.get('isCookieAllowed') === 'true' ? true : false;
   },
 };
 
