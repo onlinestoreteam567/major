a99df5835d759786e1bab8afd6b9b393c67f57fc
FreshLemur <157699166+FreshLemur@users.noreply.github.com>
2024-10-24 10:20:01 +0300

    Refactored header, added new icon and translate for header,

---
+++ client/src/components/Footer/LeftSection.jsx
@@ -0,0 +1,43 @@
+import cl from './index.module.scss';
+import logoIcon from '../../assets/svg/footer/footerLogo.svg';
+import telegramIcon from '../../assets/svg/footer/telegram.svg';
+import instagramIcon from '../../assets/svg/footer/instagram.svg';
+import facebookIcon from '../../assets/svg/footer/facebook.svg';
+import gmailIcon from '../../assets/svg/footer/gmail.svg';
+import { useTranslation } from 'react-i18next';
+
+const LeftSection = () => {
+  const { t } = useTranslation();
+
+  return (
+    <section className={cl.logoAndSocialsSection}>
+      <img src={logoIcon} alt={t('logo alt', { ns: 'footer' })} />
+      <section>
+        <p>{t('socials', { ns: 'footer' })}</p>
+        <ul>
+          <li>
+            <a href="">
+              <img src={telegramIcon} alt={t('telegram alt', { ns: 'footer' })} />
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <img src={instagramIcon} alt={t('instagram alt', { ns: 'footer' })} />
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <img src={facebookIcon} alt={t('facebook alt', { ns: 'footer' })} />
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <img src={gmailIcon} alt={t('mail alt', { ns: 'footer' })} />
+            </a>
+          </li>
+        </ul>
+      </section>
+    </section>
+  );
+};
+export default LeftSection;
---
+++ client/src/components/Footer/Navigation.jsx
@@ -0,0 +1,42 @@
+import cl from './index.module.scss';
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+
+const Navigation = () => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <section className={cl.firstNavigationSection}>
+        <ul className={cl.ulNavigation}>
+          <li className={cl.textToRight}>
+            <Link to="/">{t('main', { ns: 'footer' })}</Link>
+          </li>
+          <li className={cl.textToRight}>
+            <Link to="catalog">{t('catalog', { ns: 'footer' })}</Link>
+          </li>
+          <li className={cl.textToRight}>
+            <Link to="/about">{t('about', { ns: 'footer' })}</Link>
+          </li>
+          <li className={cl.textToRight}>
+            <Link to="blog">{t('blog', { ns: 'footer' })}</Link>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <ul className={cl.ulNavigation}>
+          <li>
+            <Link to="cooperation">{t('cooperation', { ns: 'footer' })}</Link>
+          </li>
+          <li>
+            <a href="">{t('exchange and return', { ns: 'footer' })}</a>
+          </li>
+          <li>
+            <a href="">{t('payment and delivery', { ns: 'footer' })}</a>
+          </li>
+        </ul>
+      </section>
+    </>
+  );
+};
+export default Navigation;
