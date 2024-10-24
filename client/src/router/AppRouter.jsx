82d1f599664f4f2f9c4ca3fd89b58a1ddc7c4e34
liubomyr <liubomyrpoland@gmail.com>
2024-10-24 22:49:53 +0200

    Changed imports in router v2

--- client/src/router/AppRouter.jsx
+++ client/src/router/AppRouter.jsx
@@ -6,9 +6,9 @@ import AuthLayout from './layouts/AuthLayout';
 import ProfilePage from '../pages/ProfilePage';
 
 //test-components
-import Redux from './testComponents/redux';
-import I18next from './testComponents/i18next';
-import CookieTest from './testComponents/cookie';
+import Redux from './testComponents/Redux';
+import I18next from './testComponents/I18next';
+import CookieTest from './testComponents/Cookie';
 
 // Error Boundary
 import ErrorBoundary from './error/ErrorBoundary'; // A component to handle errors
