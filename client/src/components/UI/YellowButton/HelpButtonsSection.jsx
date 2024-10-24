9498d9c5ae9009b972c9d7467d73c94396b13f72
FreshLemur <157699166+FreshLemur@users.noreply.github.com>
2024-10-24 08:13:50 +0300

    Added automatic show button "Потрібна допомога?".

--- client/src/components/UI/YellowButton/HelpButtonsSection.jsx
+++ client/src/components/UI/YellowButton/HelpButtonsSection.jsx
@@ -1,21 +1,52 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import cl from './index.module.scss';
 
 const HelpButtonsSection = ({ handleShowAnotherIcons }) => {
   const [showNeedHelpButton, setShowNeedHelpButton] = useState(false);
+  const [showHelpButtonCloseAnimation, setShowHelpButtonCloseAnimation] = useState(false);
 
   const showNeedHelpButtonFnc = () => {
     setShowNeedHelpButton(true);
   };
 
+  const closeHelpButtonAnimation = () => {
+    setShowHelpButtonCloseAnimation(true);
+    clearTimeout();
+    setTimeout(() => {
+      setShowHelpButtonCloseAnimation(false);
+      setShowNeedHelpButton(false);
+    }, 275);
+  };
+
+  // Show "Потрібна допомога?" button after 40-50 seconds after page load and close it after 3 seconds
+  useEffect(() => {
+    const randomShowDelay = Math.floor(Math.random() * 10000) + 40000;
+    const showTimer = setTimeout(() => {
+      showNeedHelpButtonFnc();
+      const hideTimer = setTimeout(() => {
+        closeHelpButtonAnimation();
+      }, 3000);
+      return () => clearTimeout(hideTimer);
+    }, randomShowDelay);
+    return () => clearTimeout(showTimer);
+  }, []);
+
   return (
-    <section className={cl.buttonsSection}>
+    <section
+      className={cl.buttonsSection}
+      onMouseLeave={() => {
+        closeHelpButtonAnimation();
+      }}
+    >
       <button className={cl.questionButton} onMouseEnter={showNeedHelpButtonFnc}>
         ?
       </button>
 
       {showNeedHelpButton && (
-        <button className={cl.needHelpButton} onClick={handleShowAnotherIcons}>
+        <button
+          className={`${cl.needHelpButton} ${showHelpButtonCloseAnimation ? cl.closeAnimation : ''}`}
+          onClick={handleShowAnotherIcons}
+        >
           Потрібна допомога?
           <div className={cl.triangle}></div>
         </button>
