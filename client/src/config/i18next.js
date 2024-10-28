8e9f82037193971cee56ae4898f8ae4301db14a4
Dmytro <dmytrolymarv@gmail.com>
2024-10-28 13:32:45 +0200

    Added bread crumbs

--- client/src/config/i18next.js
+++ client/src/config/i18next.js
@@ -11,7 +11,7 @@ i18n
     backend: {
       loadPath: '/locales/{{lng}}/{{ns}}.json',
     },
-    ns: ['common', 'header', 'catalog', 'footer', 'yellowButton', 'majorInfo', 'whyChooseUs'],
+    ns: ['common', 'header', 'breadCrumbs', 'catalog', 'footer', 'yellowButton', 'majorInfo', 'whyChooseUs'],
     defaultNS: 'common',
     interpolation: {
       escapeValue: false,
