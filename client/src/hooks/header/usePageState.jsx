bdd8d21bf6906d68b38c31e0efa1aad86c606fc2
FreshLemur <dimalimaredmi@gmail.com>
2024-11-11 09:04:11 +0200

    Deleted unnecessary console.log

--- client/src/hooks/header/usePageState.jsx
+++ client/src/hooks/header/usePageState.jsx
@@ -4,9 +4,6 @@ import { useEffect, useState } from 'react';
 export const usePageState = () => {
   const location = useLocation();
   const [isMainPage, setIsMainPage] = useState(location.pathname === '/');
-
-  console.log(123);
-
   useEffect(() => {
     setIsMainPage(location.pathname === '/');
   }, [location]);
