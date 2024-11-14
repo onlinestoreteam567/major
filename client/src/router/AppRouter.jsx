2695463dc22e0ee6c12a4146607e6f6c7c5a1b48
liubomyr <liubomyrpoland@gmail.com>
2024-11-14 17:20:57 +0100

    test v2

--- client/src/router/AppRouter.jsx
+++ client/src/router/AppRouter.jsx
@@ -38,6 +38,7 @@ const router = createBrowserRouter([
       { path: 'blog', element: <Blog /> },
       { path: 'contact', element: <Contact /> },
       { path: 'cooperation', element: <Cooperation /> },
+      { path: 'cookie', element: <CookieTest /> },
     ],
   },
   {
