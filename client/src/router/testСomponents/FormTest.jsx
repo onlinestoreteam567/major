c6255afad22bb7feefd504891699920b79521a1b
liubomyr <liubomyrpoland@gmail.com>
2024-11-19 18:14:51 +0100

    Added test component for react-hook-form + yup

--- client/src/router/testСomponents/FormTest.jsx
+++ client/src/router/testСomponents/FormTest.jsx
@@ -1,16 +1,21 @@
-import { Form } from '@components/form-components/Form/Form';
-import { Input } from '@components/form-components/Input/Input';
-import { Select } from '@components/form-components/Select/Select';
+import { Form, ErrorMessage, Input, FormGroup } from '@components/form-components';
+import { reviewSchema } from '@validations/reviewSchema';
+
+// import { Select } from '@components/form-components/Select/Select';
 
 const FormExample = () => {
   const onSubmit = (data) => alert('good' + data.sex);
   return (
     <>
       <h1 style={{ color: 'black' }}>Smart Form Component</h1>
-      <Form onSubmit={onSubmit}>
-        <Input name="firstName" />
-        <Input name="lastName" />
-        <Select name="sex" options={['female', 'male']} />
+      <Form onSubmit={onSubmit} schema={reviewSchema}>
+        <FormGroup>
+          <Input name="rating" />
+          <ErrorMessage name="rating" />
+        </FormGroup>
+
+        <Input name="comment" />
+        {/* <Select name="sex" options={['female', 'male']} /> */}
 
         <button>Submit</button>
       </Form>
