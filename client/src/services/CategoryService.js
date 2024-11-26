1cb4e8db2191279947eeb7a9b7486b9691d4cd1c
liubomyr <liubomyrpoland@gmail.com>
2024-11-26 22:47:50 +0100

    Fixed syntax error

--- client/src/services/CategoryService.js
+++ client/src/services/CategoryService.js
@@ -14,13 +14,13 @@ const ProductService = {
   async createCategory(categoryName) {
     try {
       if (!categoryName) {
-        throw new Error('Invalid category data'); 
+        throw new Error('Invalid category data');
       }
-      return await apiClient.post('/products/product_purpose_categories/', { name: categoryName }); 
+      return await apiClient.post('/products/product_purpose_categories/', { name: categoryName });
     } catch (error) {
-      handleApiError(error); 
+      handleApiError(error);
     }
-  }
+  },
 
   async getCategoryById(id) {
     try {
