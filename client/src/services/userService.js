b97d868f38225e127ce10ac0fc5a01d6306488bc
liubomyr <liubomyrpoland@gmail.com>
2024-11-29 20:32:59 +0100

    dsda

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,10 +1,26 @@
 import apiClient from '@config/apiClient';
-import validateUserData from './lib/validateUserData';
 
 const USER_ENDPOINT = import.meta.env.VITE_USER_ENDPOINT;
 
 if (!USER_ENDPOINT) {
   console.error('USER_ENDPOINT is not defined! Check the configuration in the .env file.');
+}
+
+function validateUserData(data) {
+  const { email, first_name, last_name, role } = data;
+  if (
+    !email ||
+    typeof email !== 'string' ||
+    !first_name ||
+    typeof first_name !== 'string' ||
+    !last_name ||
+    typeof last_name !== 'string' ||
+    typeof role !== 'number'
+  ) {
+    throw new Error(
+      'Invalid data structure. Expected email (string), first_name (string), last_name (string), and role (number).'
+    );
+  }
 }
 
 const UserService = {
