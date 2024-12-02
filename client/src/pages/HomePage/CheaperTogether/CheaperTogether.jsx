836f99bf146ff03efccf4445f2d3fce7151aab25
FreshLemur <dimalimaredmi@gmail.com>
2024-12-02 06:35:49 +0000

    Resolved https://projectmajor.atlassian.net/browse/CCS-102

--- client/src/pages/HomePage/CheaperTogether/CheaperTogether.jsx
+++ client/src/pages/HomePage/CheaperTogether/CheaperTogether.jsx
@@ -2,11 +2,13 @@ import cl from './index.module.scss';
 import slidesData from './data';
 import Banner from '@UI/Banner/Banner';
 import Heading from '@UI/Texts/Heading/Heading';
+import useTranslationNamespace from '@hooks/useTranslationNamespace';
 
 const CheaperTogether = () => {
+  const { getTranslation } = useTranslationNamespace('common');
   return (
     <section className={`slider-container ${cl.cheaperTogetherWrapper}`}>
-      <Heading type="h2">Разом дешевше</Heading>
+      <Heading type="h2">{getTranslation('cheaperTogether')}</Heading>
 
       <Banner slidesData={slidesData} />
     </section>
