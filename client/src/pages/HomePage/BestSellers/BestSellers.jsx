61eec461ef3db3860e586606f570a1ba8155863e
FreshLemur <dimalimaredmi@gmail.com>
2024-12-02 06:34:21 +0000

    Resolved https://projectmajor.atlassian.net/browse/CCS-101

--- client/src/pages/HomePage/BestSellers/BestSellers.jsx
+++ client/src/pages/HomePage/BestSellers/BestSellers.jsx
@@ -2,11 +2,13 @@ import cl from './index.module.scss';
 import slidesData from './data';
 import Banner from '@UI/Banner/Banner';
 import Heading from '@UI/Texts/Heading/Heading';
+import useTranslationNamespace from '@hooks/useTranslationNamespace';
 
 const BestSellers = () => {
+  const { getTranslation } = useTranslationNamespace('common');
   return (
     <section className={`slider-container ${cl.bestSellersWrapper}`}>
-      <Heading type="h2">Хіти продажу</Heading>
+      <Heading type="h2">{getTranslation('bestSellers')}</Heading>
 
       <Banner slidesData={slidesData} />
     </section>
