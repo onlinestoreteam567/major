158837162f15eb782a2a60fcb40deb4f6500f048
FreshLemur <dimalimaredmi@gmail.com>
2024-12-03 09:31:52 +0000

    Added MainPopUp translation

--- client/src/pages/HomePage/YellowButton/popUpData.js
+++ client/src/pages/HomePage/YellowButton/popUpData.js
@@ -1,10 +1,17 @@
 const popUpData = {
   type: 'hasQuestion',
-  heading: 'Залиште Вашу контактну інформацію і ми Вам зателефонуємо!',
-  subtitle: 'Маєте питання?',
-  textAreaTitle: 'Питання',
-  textAreaPlaceholder: 'Напишіть нам Ваше питання',
-  buttonText: 'Відправити повідомлення',
+  heading: 'heading',
+  subtitle: 'subtitle',
+  textAreaTitle: 'textAreaTitle',
+  textAreaPlaceholder: 'textAreaPlaceholder',
+  buttonText: 'buttonText',
+
+  typ1e: 'hasQuestion',
+  heading1: 'Залиште Вашу контактну інформацію і ми Вам зателефонуємо!',
+  subtitle1: 'Маєте питання?',
+  textAreaTitle1: 'Питання',
+  textAreaPlaceholder1: 'Напишіть нам Ваше питання',
+  buttonText1: 'Відправити повідомлення',
 };
 
 export default popUpData;
