07ab2ec61d2118e538c183b7ac7e4ea1204abdcd
FreshLemur <dimalimaredmi@gmail.com>
2024-12-11 17:37:35 +0000

    Fixed https://projectmajor.atlassian.net/browse/CCS-126

--- client/src/components/UI/CardCatalog/Container.jsx
+++ client/src/components/UI/CardCatalog/Container.jsx
@@ -2,8 +2,6 @@ import CardCatalog from './CardCatalog';
 import cl from './index.module.scss';
 import cards from './list.json';
 
-// ****** Це тимчасовий компонент *****
-
 export default function Container() {
   return (
     <ul className={cl.container}>
