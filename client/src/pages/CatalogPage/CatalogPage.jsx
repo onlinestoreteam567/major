b697691eb19d7146307259a85e7de5deaf99d4b6
FreshLemur <dimalimaredmi@gmail.com>
2024-12-12 08:37:32 +0000

    Added responsive products inside Catalog design, refactored Aside as wrapper, for future

--- client/src/pages/CatalogPage/CatalogPage.jsx
+++ client/src/pages/CatalogPage/CatalogPage.jsx
@@ -3,6 +3,10 @@ import Aside from './Aside/Aside';
 import Top from './Top/Top';
 import Container from '@UI/CardCatalog/Container';
 import TopLink from '@components/UI/TopLink/TopLink';
+import Assignment from './Aside/Assignment';
+import Range from './Aside/Range';
+import Category from './Aside/Category';
+import Switchs from './Aside/Switchs';
 
 const Catalog = () => {
   return (
@@ -12,7 +16,12 @@ const Catalog = () => {
 
       <section className={cl.mainWrapper}>
         <div className={cl.wrapAside}>
-          <Aside />
+          <Aside>
+            <Switchs />
+            <Assignment />
+            <Range />
+            <Category />
+          </Aside>
         </div>
 
         <Container />
