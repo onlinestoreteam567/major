6367b7659f43d7b4c40f4756a362cfa25ed46f73
FreshLemur <dimalimaredmi@gmail.com>
2024-12-13 09:52:11 +0000

    Refactored Navigation and maked shorter

--- client/src/components/Header/Navigation/Navigation.jsx
+++ client/src/components/Header/Navigation/Navigation.jsx
@@ -2,34 +2,19 @@ import useTranslationNamespace from '@hooks/useTranslationNamespace';
 import cl from './index.module.scss';
 import { NavLink } from 'react-router-dom';
 
-const links = [
-  {
-    text: 'catalog',
-  },
-  {
-    text: 'about',
-  },
-  {
-    text: 'blog',
-  },
-  {
-    text: 'cooperation',
-  },
-  {
-    text: 'contact',
-  },
-];
+const links = ['catalog', 'about', 'blog', 'cooperation', 'contact'];
 
 const Navigation = () => {
   const { getTranslation } = useTranslationNamespace('header');
   return (
     <nav className={cl.headerNavigation}>
-      {links.map(({ path, text }) => (
-        <NavLink key={path} to={`/${text}`} className={({ isActive }) => (isActive ? cl.activeLink : '')}>
+      {links.map((text) => (
+        <NavLink key={text} to={`/${text}`} className={({ isActive }) => (isActive ? cl.activeLink : '')}>
           {getTranslation(text)}
         </NavLink>
       ))}
     </nav>
   );
 };
+
 export default Navigation;
