1610bbaf1ec82a2af84281328d7f8a3416fef822
FreshLemur <dimalimaredmi@gmail.com>
2024-12-16 08:55:09 +0000

    Deleted unnecessary console.log

--- client/src/pages/CatalogPage/Aside/Aside.jsx
+++ client/src/pages/CatalogPage/Aside/Aside.jsx
@@ -1,17 +1,7 @@
 import cl from './index.module.scss';
-// import Assignment from './Assignment';
-// import Range from './Range';
-// import Category from './Category';
-// import Switchs from './Switchs';
 
 const Aside = ({ children }) => {
   return <aside className={cl.aside}>{children}</aside>;
 };
 
-{
-  /* <Switchs />
-<Assignment />
-<Range />
-<Category /> */
-}
 export default Aside;
