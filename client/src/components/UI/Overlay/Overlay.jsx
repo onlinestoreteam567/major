eb61dbf548fd558af3141352475186ed9f80f3d9
FreshLemur <dimalimaredmi@gmail.com>
2025-01-22 16:18:28 +0000

    Add smooth close animation when Overlay removed from DOM

--- client/src/components/UI/Overlay/Overlay.jsx
+++ client/src/components/UI/Overlay/Overlay.jsx
@@ -1,6 +1,16 @@
+import { useState } from 'react';
 import cl from './index.module.scss';
 
 const Overlay = ({ handleClose }) => {
-  return <div className={cl.overlay} onClick={handleClose}></div>;
+  const [isCloseAnimation, setIsCloseAnimation] = useState(false);
+
+  const handleCloseOverlay = () => {
+    handleClose();
+    setIsCloseAnimation(true);
+  };
+
+  return (
+    <div className={`${cl.overlay} ${isCloseAnimation ? cl.closeAnimation : ''}`} onClick={handleCloseOverlay}></div>
+  );
 };
 export default Overlay;
