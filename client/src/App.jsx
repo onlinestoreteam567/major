e995725e10f1ab2fd54a06532a0ce2254182368c
FreshLemur <dimalimaredmi@gmail.com>
2025-01-29 12:21:20 +0000

    Moved request from App component to HomePage component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,6 @@
-import { useDispatch } from 'react-redux';
 import AppRouter from './router/AppRouter';
-import { useEffect } from 'react';
-import { fetchBestSellers } from '@features/products/bestSellersSlice';
-import { useTranslation } from 'react-i18next';
 
 function App() {
-  const dispatch = useDispatch();
-  const { i18n } = useTranslation();
-
-  useEffect(() => {
-    dispatch(fetchBestSellers());
-  }, [dispatch, i18n.language]);
-
   return <AppRouter />;
 }
 
