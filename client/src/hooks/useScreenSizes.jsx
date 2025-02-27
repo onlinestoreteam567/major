36959062f10988d70836be8c0f391728566be1d3
Fresh Lemur <dimalimaredmi@gmail.comclear>
2025-02-27 16:44:21 +0200

    Added better mobile adaptation for Aside

--- client/src/hooks/useScreenSizes.jsx
+++ client/src/hooks/useScreenSizes.jsx
@@ -3,22 +3,25 @@ import useMediaQuery from '@hooks/useMediaQuery';
 
 const useScreenSizes = () => {
   const [screenSizes, setScreenSizes] = useState({
+    mobile: false,
     tablet: false,
     deskmin: false,
     deskmax: false,
   });
 
+  const isMobile = useMediaQuery('(min-width: 380px) and (max-width: 767px)');
   const isTablet = useMediaQuery('(min-width: 768px) and (max-width: 1023px)');
   const isDeskmin = useMediaQuery('(min-width: 1024px) and (max-width: 1439px)');
   const isDeskmax = useMediaQuery('(min-width: 1440px)');
 
   useEffect(() => {
     setScreenSizes({
+      mobile: isMobile,
       tablet: isTablet,
       deskmin: isDeskmin,
       deskmax: isDeskmax,
     });
-  }, [isTablet, isDeskmin, isDeskmax]);
+  }, [isMobile, isTablet, isDeskmin, isDeskmax]);
 
   return screenSizes;
 };
