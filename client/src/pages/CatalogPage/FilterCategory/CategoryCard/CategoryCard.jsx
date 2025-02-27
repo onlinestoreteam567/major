9c3a85d15a3d4f9bbc709545525e3429fd0cfe40
Fresh Lemur <dimalimaredmi@gmail.comclear>
2025-02-27 10:11:07 +0200

    Added the same logic for FilterByCategory as for FilterByStatus

--- client/src/pages/CatalogPage/FilterCategory/CategoryCard/CategoryCard.jsx
+++ client/src/pages/CatalogPage/FilterCategory/CategoryCard/CategoryCard.jsx
@@ -1,21 +1,37 @@
 import cl from './index.module.scss';
-import { getProductsByCategory } from '@redux/products/service';
-import { setCategory } from '@redux/filter/filterSlice';
-import { useDispatch } from 'react-redux';
+import { fetchProductsAll, getProductsByCategory } from '@redux/products/service';
+import { resetFilter, setCategory } from '@redux/filter/filterSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { filterCategory } from '@redux/selectors';
 
 const CategoryCard = ({ item }) => {
   const dispatch = useDispatch();
+  const newCategory = useSelector(filterCategory);
+
   const getCategory = (value) => {
-    dispatch(setCategory(value));
-    dispatch(getProductsByCategory(value));
+    if (newCategory === value) {
+      dispatch(resetFilter());
+      dispatch(fetchProductsAll());
+    } else {
+      dispatch(setCategory(value));
+      dispatch(getProductsByCategory(value));
+    }
   };
 
   return (
-    <li className={cl.categoryCard} id={item.id} onClick={(e) => getCategory(e.currentTarget.id)}>
-      <figure>
-        <img src={item.image} alt={item.name} />
-        <figcaption>{item.name}</figcaption>
-      </figure>
+    <li className={cl.categoryCard} id={item.id}>
+      <label>
+        <figure>
+          <img src={item.image} alt={item.name} className={newCategory === item.id ? cl.active : ''} />
+          <figcaption>{item.name}</figcaption>
+        </figure>
+        <input
+          type="checkbox"
+          name="category"
+          checked={newCategory === item.id}
+          onChange={() => getCategory(item.id)}
+        />
+      </label>
     </li>
   );
 };
