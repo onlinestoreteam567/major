ed53afa5ed99c7d61706701b2ae14be9357ce162
Fresh Lemur <dimalimaredmi@gmail.comclear>
2025-03-04 15:35:05 +0200

    Resolved Catalog bug, if images is undefined

--- client/src/pages/CatalogPage/Products/CardsContainer/Card/Image/Image.jsx
+++ client/src/pages/CatalogPage/Products/CardsContainer/Card/Image/Image.jsx
@@ -4,12 +4,16 @@ import LabelSale from '../../../../../../components/UI/CardProduct/Labels/LabelS
 import cl from './index.module.scss';
 
 export default function Image({ card }) {
-  return (
+  return card.images && card.images.length > 0 ? (
     <div className={cl.wrapImg}>
       {card.is_best_seller ? <LabelHit /> : ''}
       {card.is_new ? <LabelNew /> : ''}
       {card.is_discount ? <LabelSale card={card} /> : ''}
       <img src={card.images[0].image} alt="" />
     </div>
+  ) : (
+    <h1 style={{ textAlign: 'center', color: 'red', fontSize: '20px' }}>
+      Дуже класна заглушка яка тут буде якщо фото не прилетить або хтось забуде поставити
+    </h1>
   );
 }
