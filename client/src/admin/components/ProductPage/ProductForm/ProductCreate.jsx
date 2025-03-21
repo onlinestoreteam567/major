6f132491caf87788e7c1f36c450cebb79e9d0049
Fresh Lemur <dimalimaredmi@gmail.comclear>
2025-03-21 09:58:42 +0200

    Refactoring: Added custom ProductForm component with all form fields

--- client/src/admin/components/ProductPage/ProductForm/ProductCreate.jsx
+++ client/src/admin/components/ProductPage/ProductForm/ProductCreate.jsx
@@ -3,13 +3,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { errorCreateProduct, loadCreateProduct, responseCreateProduct } from '../../../redux/selectors';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { Input, Textarea } from '@components/form-components';
 import { productSchema } from '../../../validations/productSchema';
-import CheckBox from '@components/form-components/Checkbox/Checkbox';
-import CategorySelect from './CategorySelect';
-import TypeSelect from './TypeSelect';
-import ImageUpload from './ImageUpload';
 import ErrorText from '../../ErrorText/ErrorText';
+import ProductForm from './ProductForm';
 
 const ProductCreate = () => {
   const dispatch = useDispatch();
@@ -50,38 +46,17 @@ const ProductCreate = () => {
       }
     });
 
-    // Dispatch create product action
     dispatch(createProduct(formData));
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} style={{ display: 'flex', flexDirection: 'column', color: 'black' }}>
-      <Input labelText="Article:" name="article" register={register} errors={errors} />
-      <CheckBox labelText="В наявності" name="available" register={register} />
-      <CheckBox labelText="Хіт продажу" name="is_best_seller" register={register} />
-      <CheckBox labelText="Новинка" name="is_new" register={register} />
-      <Input labelText="Product Name (UK):" name="product_name_uk" register={register} errors={errors} />
-      <Input labelText="Product Name (EN):" name="product_name_en" register={register} errors={errors} />
-      <Input type="number" labelText="Price:" name="price" register={register} errors={errors} />
-      <Input type="number" labelText="Discount:" name="discount" register={register} errors={errors} />
-      <Input type="number" labelText="Volume (ml):" name="volume_ml" register={register} errors={errors} />
-      <Textarea labelText="Description (UK):" name="description_uk" register={register} errors={errors} />
-      <Textarea labelText="Description (EN):" name="description_en" register={register} errors={errors} />
-      <Textarea labelText="Ingredients:" name="ingredients" register={register} errors={errors} />
-      <Textarea labelText="Application (UK):" name="application_uk" register={register} errors={errors} />
-      <Textarea labelText="Application (EN):" name="application_en" register={register} errors={errors} />
-      <CategorySelect control={control} errors={errors} />
-      <TypeSelect control={control} errors={errors} />
-      <ImageUpload control={control} />
-
+      <ProductForm register={register} errors={errors} control={control} />
       <button type="submit" disabled={isLoading}>
         {isLoading ? 'Creating...' : 'Create Product'}
       </button>
-
       {errorPost && <ErrorText error={errorPost}></ErrorText>}
-
-      {/* Success response */}
-      {response && <p style={{ color: 'green' }}>Product added successfully!</p>}
+      {response && <p style={{ color: 'green' }}>Успішно створено товар!</p>}
     </form>
   );
 };
--- client/src/admin/components/ProductPage/ProductForm/ProductEdit.jsx
+++ client/src/admin/components/ProductPage/ProductForm/ProductEdit.jsx
@@ -4,18 +4,14 @@ import { getProductById } from '@redux/products/service';
 import { errorEditProduct, loadEditProduct, responseEditProduct } from '../../../redux/selectors';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { Input, Textarea } from '@components/form-components';
 import { productSchema } from '../../../validations/productSchema';
-import CheckBox from '@components/form-components/Checkbox/Checkbox';
 import { useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import { loadProductId, selectProductId } from '@redux/selectors';
 import Spinner from '@components/helpers/Spinner';
 import cl from './index.module.scss';
-import CategorySelect from './CategorySelect';
-import TypeSelect from './TypeSelect';
-import ImageUpload from './ImageUpload';
 import ErrorText from '../../ErrorText/ErrorText';
+import ProductForm from './ProductForm';
 
 const ProductEdit = () => {
   const location = useLocation();
@@ -101,25 +97,7 @@ const ProductEdit = () => {
         <Spinner />
       ) : (
         <form onSubmit={handleSubmit(onSubmit)} style={{ display: 'flex', flexDirection: 'column', color: 'black' }}>
-          <Input labelText="Article:" name="article" register={register} errors={errors} />
-          <CheckBox labelText="В наявності" name="available" register={register} />
-          <CheckBox labelText="Хіт продажу" name="is_best_seller" register={register} />
-          <CheckBox labelText="Новинка" name="is_new" register={register} />
-          <Input labelText="Product Name (UK):" name="product_name_uk" register={register} errors={errors} />
-          <Input labelText="Product Name (EN):" name="product_name_en" register={register} errors={errors} />
-          <Input type="number" labelText="Price:" name="price" register={register} errors={errors} />
-          <Input type="number" labelText="Discount:" name="discount" register={register} errors={errors} />
-          <Input type="number" labelText="Volume (ml):" name="volume_ml" register={register} errors={errors} />
-          <Textarea labelText="Description (UK):" name="description_uk" register={register} errors={errors} />
-          <Textarea labelText="Description (EN):" name="description_en" register={register} errors={errors} />
-          <Textarea labelText="Ingredients:" name="ingredients" register={register} errors={errors} />
-          <Textarea labelText="Application (UK):" name="application_uk" register={register} errors={errors} />
-          <Textarea labelText="Application (EN):" name="application_en" register={register} errors={errors} />
-
-          <CategorySelect control={control} errors={errors} />
-          <TypeSelect control={control} errors={errors} />
-          <ImageUpload control={control} />
-
+          <ProductForm register={register} errors={errors} control={control} />
           {responseGet && responseGet.images && responseGet.images.length > 0 && (
             <ul className={cl.images}>
               Images
