9184b67179ac2cea99b2fc40ab87d7a6c88d85ff
lemur <dimalimaredmi@gmail.com>
2025-04-03 22:01:21 +0300

    feature: implemented skeleton for mainBanner for better CLS

---
+++ client/src/components/UI/Skeleton/Skeleton.jsx
@@ -0,0 +1,23 @@
+import Spinner from '@components/helpers/Spinner';
+import cl from './index.module.scss';
+import useScreenSizes from '@hooks/useScreenSizes';
+
+const Skeleton = ({ width = '100%', heights = { extraMobile, mobile, tablet, deskmin, deskmax } }) => {
+  const { mobile, tablet, deskmin, deskmax } = useScreenSizes();
+
+  const getHeight = () => {
+    if (deskmax) return heights.deskmax;
+    if (deskmin) return heights.deskmin;
+    if (tablet) return heights.tablet;
+    if (mobile) return heights.mobile;
+    return heights.extraMobile;
+  };
+
+  return (
+    <div className={cl.skeleton} style={{ width, height: getHeight() }} >
+      <Spinner />
+    </div>
+  );
+};
+
+export default Skeleton;
