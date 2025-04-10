170d7e3e6de60d9f8e9ff202f9f2ba589fa1a7b6
lemur <dimalimaredmi@gmail.com>
2025-04-10 10:46:59 +0300

    feature: implemented functionality to set minPrice and maxPrice for priceRange in catalog filters

--- client/src/pages/CatalogPage/Aside/FilterPrice/PriceRange.jsx
+++ client/src/pages/CatalogPage/Aside/FilterPrice/PriceRange.jsx
@@ -4,17 +4,34 @@ import useTranslationNamespace from '@hooks/useTranslationNamespace';
 import RangeSlider from './RangeSlider';
 import { useDispatch, useSelector } from 'react-redux';
 import { getProductsByPrice } from '@redux/products/service';
-import { filterPrice } from '@redux/selectors';
+import { filterPrice, selectProducts } from '@redux/selectors';
 import { setPrice } from '@redux/filter/filterSlice';
 
 const PriceRange = () => {
   const newPrice = useSelector(filterPrice);
+  const products = useSelector(selectProducts);
   const [minPrice, setMinPrice] = useState(newPrice.min);
   const [maxPrice, setMaxPrice] = useState(newPrice.max);
+  const [maxLimit, setMaxLimit] = useState(999);
   const priceGap = 1;
-  const maxLimit = 999;
   const dispatch = useDispatch();
   const progressRef = useRef(null);
+
+  useEffect(() => {
+    if (products && products.length > 0) {
+      const pricesWithDiscounts = products.map((product) => {
+        if (product.discount) {
+          return product.price - (product.price * product.discount) / 100;
+        }
+        return product.price;
+      });
+      const minProductPrice = Math.min(...pricesWithDiscounts);
+      const maxProductPrice = Math.max(...pricesWithDiscounts);
+      setMinPrice(Math.floor(minProductPrice));
+      setMaxPrice(Math.ceil(maxProductPrice));
+      setMaxLimit(Math.ceil(maxProductPrice));
+    }
+  }, [products]);
 
   useEffect(() => {
     setMinPrice(newPrice.min);
@@ -26,7 +43,7 @@ const PriceRange = () => {
       progressRef.current.style.left = (minPrice / maxLimit) * 100 + '%';
       progressRef.current.style.right = 100 - (maxPrice / maxLimit) * 100 + '%';
     }
-  }, [minPrice, maxPrice]);
+  }, [minPrice, maxPrice, maxLimit]);
 
   const handleMinInputChange = (e) => {
     const value = parseInt(e.target.value);
@@ -39,6 +56,12 @@ const PriceRange = () => {
     const value = parseInt(e.target.value);
     if (value <= maxLimit && value - minPrice >= priceGap) {
       setMaxPrice(value);
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      getByPrice();
     }
   };
 
@@ -67,11 +90,11 @@ const PriceRange = () => {
       <div className={cl.priceInput}>
         <div className={cl.field}>
           <span>{getTranslation('from')}</span>
-          <input type="number" value={minPrice} onChange={handleMinInputChange} />
+          <input type="number" value={minPrice} onChange={handleMinInputChange} onKeyDown={handleKeyPress} />
         </div>
         <div className={cl.field}>
           <span>{getTranslation('to')}</span>
-          <input type="number" value={maxPrice} onChange={handleMaxInputChange} />
+          <input type="number" value={maxPrice} onChange={handleMaxInputChange} onKeyDown={handleKeyPress} />
         </div>
         <button onClick={getByPrice}>{getTranslation('ok')}</button>
       </div>
