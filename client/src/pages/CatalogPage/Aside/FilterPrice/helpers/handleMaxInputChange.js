ab974fd8ea48c0bbffd95a5eeaf73c50c1ebe08e
lemur <dimalimaredmi@gmail.com>
2025-04-11 09:02:06 +0300

    fix: https://projectmajor.atlassian.net/browse/CCS-222

--- client/src/pages/CatalogPage/Aside/FilterPrice/helpers/handleMaxInputChange.js
+++ client/src/pages/CatalogPage/Aside/FilterPrice/helpers/handleMaxInputChange.js
@@ -1,12 +1,17 @@
+const digitRegex = /^\d*$/;
+
 export const handleMaxInputChange = (e, setMaxInputValue, setMaxPrice) => {
   const value = e.target.value;
-  setMaxInputValue(value);
-  if (value === '') {
-    setMaxPrice(0);
-  } else if (value === '0') {
-    setMaxInputValue('');
-  } else if (!isNaN(value)) {
-    const numValue = parseInt(value);
-    setMaxPrice(numValue);
+
+  if (digitRegex.test(value)) {
+    setMaxInputValue(value);
+    if (value === '') {
+      setMaxPrice(0);
+    } else if (value === '0') {
+      setMaxInputValue('');
+    } else {
+      const numValue = parseInt(value);
+      setMaxPrice(numValue);
+    }
   }
 };
