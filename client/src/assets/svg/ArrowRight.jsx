6b797fae44808dae4f0b5a01ba05f0fe72ba4457
FreshLemur <dimalimaredmi@gmail.com>
2025-04-23 17:27:37 +0300

    awdkadwk;oa;wkdo

--- client/src/assets/svg/ArrowRight.jsx
+++ client/src/assets/svg/ArrowRight.jsx
@@ -3,6 +3,7 @@ const ArrowRight = () => {
     <svg width="64" height="40" viewBox="0 0 64 40" fill="none" xmlns="http://www.w3.org/2000/svg">
       <rect width="64" height="40" rx="8" fill="#F8F8F9" />
       <path
+      awdliawdlijalwjid
         d="M34.4297 13.9297L40.4997 19.9997L34.4297 26.0697"
         stroke="#22211F"
         strokeWidth="1.5"
