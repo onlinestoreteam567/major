436b11971159b8d6add8f6434b49fee787ef5b13
Fresh Lemur <dimalimaredmi@gmail.com>
2025-05-05 10:37:25 +0300

    fix: handle image loading errors by setting a placeholder image, https://projectmajor.atlassian.net/browse/CCS-264

--- client/src/pages/CatalogPage/Products/CardsContainer/Card/Image/Image.jsx
+++ client/src/pages/CatalogPage/Products/CardsContainer/Card/Image/Image.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import LabelHit from '../../../../../ProductPage/Card/ProductDetails/Labels/LabelHit';
 import LabelNew from '../../../../../ProductPage/Card/ProductDetails/Labels/LabelNew';
 import LabelSale from '../../../../../ProductPage/Card/ProductDetails/Labels/LabelSale';
@@ -6,14 +7,16 @@ import cl from './index.module.scss';
 export default function Image({ card }) {
   const placeholderImage = '/images/placeholder.webp';
   const hasImages = card.images && card.images.length > 0;
-  const imageSrc = hasImages ? card.images[0].image : placeholderImage;
+  const [imageSrc, setImageSrc] = useState(hasImages ? card.images[0].image : placeholderImage);
+
+  const handleError = () => setImageSrc(placeholderImage);
 
   return (
     <div className={cl.wrapImg}>
       {card.is_best_seller && <LabelHit />}
       {card.is_new && <LabelNew />}
       {card.is_discount && <LabelSale card={card} />}
-      <img src={imageSrc} alt={card.name || 'Placeholder'} />
+      <img src={imageSrc} alt={card.name || 'Placeholder'} onError={handleError} />
     </div>
   );
 }
