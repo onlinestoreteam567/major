da39e7b1f8cc414b817819fc78541196b9c13c0d
Fresh Lemur <dimalimaredmi@gmail.com>
2025-05-12 12:07:38 +0300

    fix: without error

--- client/src/entry-client.jsx
+++ client/src/entry-client.jsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
 ReactDOM.hydrateRoot(
   document.getElementById('root'),
-  <StrictMode>
-    <p>123</p>
-  </StrictMode>,
-  () => {
-    console.log('Client-side hydrated HTML:', document.getElementById('root')?.innerHTML);
-  }
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
--- client/src/entry-server.jsx
+++ client/src/entry-server.jsx
@@ -1,23 +1,18 @@
-import { StrictMode } from 'react';
 import { renderToString } from 'react-dom/server';
-import fs from 'fs/promises'; // Or your preferred file system module
-import path from 'path';
+import { StaticRouter } from 'react-router-dom/server';
+import App from './App';
+import { StrictMode } from 'react';
 
 export async function render(url) {
   const rendered = renderToString(
     <StrictMode>
-      <p>123</p>
+      <StaticRouter location={url.startsWith('/') ? url : '/' + url}>
+        <App />
+      </StaticRouter>
     </StrictMode>
   );
-  console.log('Server-rendered HTML:', rendered);
-
-  // Read your index.html file
-  const indexHTML = await fs.readFile(path.resolve('./index.html'), 'utf-8');
-
-  // Inject the server-rendered HTML into the root div
-  const htmlWithRoot = indexHTML.replace('<div id="root"></div>', `<div id="root">${rendered}</div>`);
 
   return {
-    html: htmlWithRoot,
+    html: rendered,
   };
 }
