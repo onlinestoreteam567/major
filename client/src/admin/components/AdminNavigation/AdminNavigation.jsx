6f12247d5e02827771bc6b41a7a274d376c876b6
Fresh Lemur <dimalimaredmi@gmail.com>
2025-05-22 17:24:37 +0300

    feature: 320px design

--- client/src/admin/components/AdminNavigation/AdminNavigation.jsx
+++ client/src/admin/components/AdminNavigation/AdminNavigation.jsx
@@ -3,41 +3,46 @@ import { NavLink } from 'react-router-dom';
 
 const AdminNavigation = () => {
   return (
-    <nav style={{ color: 'black' }}>
+    <nav className={cl.adminNavigation} style={{ color: 'black' }}>
       <NavLink to={`/admin/products`}>
         <img src="/images/admin/navigation/Logo.png" alt="" />
       </NavLink>
-      <NavLink to={`/admin/products`}>
-        <img src="/images/admin/navigation/GoodsIcon.png" alt="" />
-      </NavLink>
-      <NavLink to={`/admin/categories`}>
-        <img src="/images/admin/navigation/Catalog icons.png" alt="" />
-      </NavLink>
-      <NavLink to={`/admin/promocodes`}>
-        <img src="/images/admin/navigation/Promo icons.png" alt="" />
-      </NavLink>
-      <NavLink to={`/admin/partners`}>
-        <img src="/images/admin/navigation/Map.png" alt="" />
-      </NavLink>
-      <button>
-        <img src="/images/admin/navigation/Articles icon.png" alt="" />
-      </button>
-      <NavLink to={`/admin/banners`}>
-        <img src="/images/admin/navigation/Banners icons.png" alt="" />
-      </NavLink>
-      <NavLink to={`/admin/reviews`}>
-        <img src="/images/admin/navigation/Rewievs icons.png" alt="" />
-      </NavLink>
-      <button>
-        <img src="/images/admin/navigation/Contact icon.png" alt="" />
-      </button>
 
-      <button>
-        <img src="/images/admin/navigation/Setting icons.png" alt="" />
-      </button>
-      <button>
-        <img src="/images/admin/navigation/Exit icons.png" alt="" />
-      </button>
+      <div>
+        <NavLink to={`/admin/products`} className={cl.active}>
+          <img src="/images/admin/navigation/Goods icon.png" alt="" />
+        </NavLink>
+        <NavLink to={`/admin/categories`}>
+          <img src="/images/admin/navigation/Catalog icons.png" alt="" />
+        </NavLink>
+        <NavLink to={`/admin/promocodes`}>
+          <img src="/images/admin/navigation/Promo icons.png" alt="" />
+        </NavLink>
+        <NavLink to={`/admin/partners`}>
+          <img src="/images/admin/navigation/Map.png" alt="" />
+        </NavLink>
+        <button>
+          <img src="/images/admin/navigation/Articles icon.png" alt="" />
+        </button>
+        <NavLink to={`/admin/banners`}>
+          <img src="/images/admin/navigation/Banners icons.png" alt="" />
+        </NavLink>
+        <NavLink to={`/admin/reviews`}>
+          <img src="/images/admin/navigation/Rewievs icons.png" alt="" />
+        </NavLink>
+        <button>
+          <img src="/images/admin/navigation/Contact icon.png" alt="" />
+        </button>
+      </div>
+
+      <div>
+        <button>
+          <img src="/images/admin/navigation/Setting icons.png" alt="" />
+        </button>
+        <button>
+          <img src="/images/admin/navigation/Exit icons.png" alt="" />
+        </button>
+      </div>
     </nav>
   );
 };
