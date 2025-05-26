e79c819e781b8b4b4d8577132a2b85d42c8aa6bd
Fresh Lemur <dimalimaredmi@gmail.com>
2025-05-26 09:51:01 +0300

    feature: implement burger menu toggle and overlay for admin navigation

--- client/src/admin/components/AdminNavigation/AdminNavigation.jsx
+++ client/src/admin/components/AdminNavigation/AdminNavigation.jsx
@@ -1,68 +1,78 @@
 import useScreenSizes from '@hooks/useScreenSizes';
 import cl from './index.module.scss';
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import Overlay from '@components/UI/Overlay/Overlay';
 
 const AdminNavigation = () => {
   const { tablet, deskmin, deskmax } = useScreenSizes();
+  const [isShowBurgerButton, setIsShowBurgerButton] = useState(false);
 
-  const isMobile = tablet || deskmin || deskmax;
+  const isDisplayNavigation = !(tablet || deskmin || deskmax || isShowBurgerButton);
+  const isMobileOverlay = !(tablet || deskmin || deskmax);
 
-  return !isMobile ? (
-    <button>
-      <img src="/svg/admin/navigation/burger.svg" />
+  const handleClose = () => setIsShowBurgerButton(false);
+
+  return isDisplayNavigation ? (
+    <button onClick={() => setIsShowBurgerButton(!isShowBurgerButton)}>
+      <img src="/svg/admin/navigation/burger.svg" alt="Burger menu icon" />
     </button>
   ) : (
-    <nav className={cl.adminNavigation} style={{ color: 'black' }}>
-      <NavLink to={`/admin/products`}>
-        <img src="/images/admin/navigation/Logo.png" />
-      </NavLink>
+    <>
+      {isMobileOverlay && <Overlay handleClose={handleClose} />}
 
-      <div>
-        <NavLink to={`/admin/products`} className={cl.active}>
-          <img src="/images/admin/navigation/Goods icon.png" />
-          <span>Товари</span>
+      <nav className={cl.adminNavigation} style={{ color: 'black' }}>
+        <NavLink to={`/admin/products`}>
+          <img src="/images/admin/navigation/Logo.png" alt="Company Logo" />
         </NavLink>
-        <NavLink to={`/admin/categories`}>
-          <img src="/images/admin/navigation/Catalog icons.png" />
-          <span>Категорії</span>
-        </NavLink>
-        <NavLink to={`/admin/promocodes`}>
-          <img src="/images/admin/navigation/Promo icons.png" />
-          <span>Промокоди</span>
-        </NavLink>
-        <NavLink to={`/admin/partners`}>
-          <img src="/images/admin/navigation/Map.png" />
-          <span>Карта</span>
-        </NavLink>
-        <button>
-          <img src="/images/admin/navigation/Articles icon.png" />
-          <span>Статті</span>
-        </button>
-        <NavLink to={`/admin/banners`}>
-          <img src="/images/admin/navigation/Banners icons.png" />
-          <span>Банери</span>
-        </NavLink>
-        <NavLink to={`/admin/reviews`}>
-          <img src="/images/admin/navigation/Rewievs icons.png" />
-          <span>Відгуки</span>
-        </NavLink>
-        <button>
-          <img src="/images/admin/navigation/Contact icon.png" />
-          <span>Контакти</span>
-        </button>
-      </div>
 
-      <div>
-        <button>
-          <img src="/images/admin/navigation/Setting icons.png" />
-          <span>Налаштування</span>
-        </button>
-        <button>
-          <img src="/images/admin/navigation/Exit icons.png" />
-          <span>Вихід</span>
-        </button>
-      </div>
-    </nav>
+        <div>
+          <NavLink to={`/admin/products`} className={cl.active}>
+            <img src="/images/admin/navigation/Goods icon.png" alt="Goods icon" />
+            <span>Товари</span>
+          </NavLink>
+          <NavLink to={`/admin/categories`}>
+            <img src="/images/admin/navigation/Catalog icons.png" alt="Catalog icon" />
+            <span>Категорії</span>
+          </NavLink>
+          <NavLink to={`/admin/promocodes`}>
+            <img src="/images/admin/navigation/Promo icons.png" alt="Promocodes icon" />
+            <span>Промокоди</span>
+          </NavLink>
+          <NavLink to={`/admin/partners`}>
+            <img src="/images/admin/navigation/Map.png" alt="Partners icon" />
+            <span>Карта</span>
+          </NavLink>
+          <button>
+            <img src="/images/admin/navigation/Articles icon.png" alt="Articles icon" />
+            <span>Статті</span>
+          </button>
+          <NavLink to={`/admin/banners`}>
+            <img src="/images/admin/navigation/Banners icons.png" alt="Banners icon" />
+            <span>Банери</span>
+          </NavLink>
+          <NavLink to={`/admin/reviews`}>
+            <img src="/images/admin/navigation/Rewievs icons.png" alt="Reviews icon" />
+            <span>Відгуки</span>
+          </NavLink>
+          <button>
+            <img src="/images/admin/navigation/Contact icon.png" alt="Contact icon" />
+            <span>Контакти</span>
+          </button>
+        </div>
+
+        <div>
+          <button>
+            <img src="/images/admin/navigation/Setting icons.png" alt="Settings icon" />
+            <span>Налаштування</span>
+          </button>
+          <button>
+            <img src="/images/admin/navigation/Exit icons.png" alt="Exit icon" />
+            <span>Вихід</span>
+          </button>
+        </div>
+      </nav>
+    </>
   );
 };
 export default AdminNavigation;
