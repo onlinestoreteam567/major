aae6c6c65e4af813e72bb059aaf984965d2e2995
Fresh Lemur <dimalimaredmi@gmail.com>
2025-05-28 08:59:21 +0300

    fix: partners translations in admin panel

--- client/src/admin/components/PartnersManagement.jsx/PartnersForm/PartnersForm.jsx
+++ client/src/admin/components/PartnersManagement.jsx/PartnersForm/PartnersForm.jsx
@@ -3,13 +3,18 @@ import { Input } from '@components/form-components';
 const PartnersForm = ({ register, errors }) => {
   return (
     <>
-      <Input labelText="name_uk" name="name_uk" register={register} errors={errors} />
-      <Input labelText="name_en" name="name_en" register={register} errors={errors} />
-      <Input labelText="addres_uk" name="addres_uk" register={register} errors={errors} />
-      <Input labelText="addres_en" name="addres_en" register={register} errors={errors} />
-      <Input labelText="work_schedule_weekdays" name="work_schedule_weekdays" register={register} errors={errors} />
-      <Input labelText="work_schedule_weekends" name="work_schedule_weekends" register={register} errors={errors} />
-      <Input labelText="google_maps_link" name="google_maps_link" register={register} errors={errors} />
+      <Input labelText="Ім'я укр" name="name_uk" register={register} errors={errors} />
+      <Input labelText="Ім'я англ" name="name_en" register={register} errors={errors} />
+      <Input labelText="Адреса укр" name="addres_uk" register={register} errors={errors} />
+      <Input labelText="Адреса англ" name="addres_en" register={register} errors={errors} />
+      <Input labelText="Графік роботи у будні дні" name="work_schedule_weekdays" register={register} errors={errors} />
+      <Input
+        labelText="Графік роботи у суботу та неділю"
+        name="work_schedule_weekends"
+        register={register}
+        errors={errors}
+      />
+      <Input labelText="Посилання на гугл мап" name="google_maps_link" register={register} errors={errors} />
       <Input labelText="Широта" name="longitude" register={register} errors={errors} />
       <Input labelText="Довгота" name="latitude" register={register} errors={errors} />
     </>
