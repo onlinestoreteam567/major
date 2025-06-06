5e1a774b8b58fc087a03362cbd61e958cc7fc61f
Stanislav <ssovelich@gmail.com>
2025-06-06 18:56:36 +0300

    DeletePopUp customization

--- client/src/admin/components/ProductManagement/ProductList/DeletePopUp/DeletePopUp.jsx
+++ client/src/admin/components/ProductManagement/ProductList/DeletePopUp/DeletePopUp.jsx
@@ -2,21 +2,31 @@ import Button from '@components/UI/Button/Button';
 import cl from './index.module.scss';
 import Overlay from '@components/UI/Overlay/Overlay';
 
-const DeletePopUp = ({ closeDeletePopUp, handleDelete, card, setDeletedItemName }) => {
+const DeletePopUp = ({
+  closeDeletePopUp,
+  handleDelete,
+  itemId,
+  itemName,
+  setDeletedItemName,
+  confirmText = 'Ви впевнені, що хочете видалити цей елемент?',
+  children,
+}) => {
   return (
     <>
       <Overlay handleClose={closeDeletePopUp} />
       <div className={cl.deletePopUp}>
-        <h2>Ви впевнені, що хочете видалити цей товар?</h2>
-        <div>
+        <h2>{confirmText}</h2>
+        {children && <div className={cl.customContent}>{children}</div>}
+
+         <div className={cl.buttons}>
           <Button onClick={closeDeletePopUp} variant="secondary">
             Ні
           </Button>
           <Button
             onClick={() => {
-              handleDelete(card.id);
+              handleDelete(itemId);
               closeDeletePopUp();
-              setDeletedItemName(card.name);
+              if (setDeletedItemName) setDeletedItemName(itemName);
             }}
           >
             Так
