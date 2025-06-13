5444fff8da533e1fd378360a05594044bf57d92c
Fresh Lemur <dimalimaredmi@gmail.com>
2025-06-13 09:31:07 +0300

    feature: input styles

--- client/src/admin/components/ProductManagement/ProductForm/ProductForm.jsx
+++ client/src/admin/components/ProductManagement/ProductForm/ProductForm.jsx
@@ -3,16 +3,44 @@ import CheckBox from '@components/form-components/Checkbox/Checkbox';
 import CategorySelect from './CategorySelect';
 import TypeSelect from './TypeSelect';
 import ImageUpload from './ImageUpload/ImageUpload';
-// import ResponsiveTextareas from '@components/form-components/ResponsiveTextareas/ResponsiveTextareas';
 
 const ProductForm = ({ register, errors, control }) => {
   return (
     <>
       <ImageUpload control={control} name="upload_images" errors={errors} />
-      <Input labelText="Назва (UA):" name="product_name_uk" register={register} errors={errors} />
-      <Input labelText="Назва (ENG):" name="product_name_en" register={register} errors={errors} />
-      <Input labelText="Артикул:" name="article" register={register} errors={errors} />
-      <Input type="number" labelText="Об’єм(ml):" name="volume_ml" register={register} errors={errors} />
+      <Input
+        placeholder="example"
+        labelText="Назва (UA):"
+        name="product_name_uk"
+        register={register}
+        errors={errors}
+        variant="admin"
+      />
+      <Input
+        placeholder="example"
+        labelText="Назва (ENG):"
+        name="product_name_en"
+        register={register}
+        errors={errors}
+        variant="admin"
+      />
+      <Input
+        placeholder="example"
+        labelText="Артикул:"
+        name="article"
+        register={register}
+        errors={errors}
+        variant="admin"
+      />
+      <Input
+        placeholder="example"
+        type="number"
+        labelText="Об’єм(ml):"
+        name="volume_ml"
+        register={register}
+        errors={errors}
+        variant="admin"
+      />
       <CheckBox labelText="В наявності" name="available" register={register} />
       <CheckBox labelText="Новинка" name="is_new" register={register} />
       <CheckBox labelText="Хіт продажу" name="is_best_seller" register={register} />
