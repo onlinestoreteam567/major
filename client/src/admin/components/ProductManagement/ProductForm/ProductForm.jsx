f0fd7ca3faf4c48f55c2a382e5bc96be4d8c54e7
Fresh Lemur <dimalimaredmi@gmail.com>
2025-06-18 12:26:08 +0300

    fix: returned form layout styles

--- client/src/admin/components/ProductManagement/ProductForm/ProductForm.jsx
+++ client/src/admin/components/ProductManagement/ProductForm/ProductForm.jsx
@@ -6,6 +6,7 @@ import ResponsiveTextareas from '@components/form-components/ResponsiveTextareas
 import AdminCheckBox from '@components/form-components/Checkbox/AdminCheckbox/Checkbox';
 import { useEffect, useState } from 'react';
 import AdminMessage from '../../AdminMessage/AdminMessage';
+import cl from './index.module.scss';
 
 const ProductForm = ({ register, errors, control, resetImagesTrigger }) => {
   const [messageText, setMessageText] = useState(null);
@@ -28,73 +29,81 @@ const ProductForm = ({ register, errors, control, resetImagesTrigger }) => {
         resetImagesTrigger={resetImagesTrigger}
         setMessageText={() => setMessageText('Фото успішно додано')}
       />
-      <Input
-        placeholder="example"
-        labelText="Назва (UA):"
-        name="product_name_uk"
-        register={register}
-        errors={errors}
-        variant="admin"
-      />
-      <Input
-        placeholder="example"
-        labelText="Назва (ENG):"
-        name="product_name_en"
-        register={register}
-        errors={errors}
-        variant="admin"
-      />
-      <Input
-        placeholder="example"
-        labelText="Артикул:"
-        name="article"
-        register={register}
-        errors={errors}
-        variant="admin"
-      />
+      <div className={cl.productBasicInfo}>
+        <Input
+          placeholder="example"
+          labelText="Назва (UA):"
+          name="product_name_uk"
+          register={register}
+          errors={errors}
+          variant="admin"
+        />
+        <Input
+          placeholder="example"
+          labelText="Назва (ENG):"
+          name="product_name_en"
+          register={register}
+          errors={errors}
+          variant="admin"
+        />
+        <Input
+          placeholder="example"
+          labelText="Артикул:"
+          name="article"
+          register={register}
+          errors={errors}
+          variant="admin"
+        />
+      </div>
 
-      <AdminCheckBox labelText="В наявності" name="available" register={register} />
-      <AdminCheckBox labelText="Новинка" name="is_new" register={register} />
-      <AdminCheckBox labelText="Хіт продажу" name="is_best_seller" register={register} />
-      <Input
-        placeholder="0"
-        type="number"
-        labelText="Об’єм(ml):"
-        name="volume_ml"
-        register={register}
-        errors={errors}
-        variant="admin"
-      />
-      <Input
-        placeholder="0"
-        variant="admin"
-        type="number"
-        labelText="Ціна (грн):"
-        name="price"
-        register={register}
-        errors={errors}
-      />
-      <Input
-        placeholder="0"
-        variant="admin"
-        type="number"
-        labelText="Знижка(%):"
-        name="discount"
-        register={register}
-        errors={errors}
-      />
-      <Input
-        placeholder="0"
-        variant="admin"
-        type="number"
-        labelText="Ціна зі знижкою:"
-        name="discounted_price"
-        register={register}
-        errors={errors}
-        readOnly
-      />
-      <CategorySelect control={control} errors={errors} />
-      <TypeSelect control={control} errors={errors} />
+      <div className={cl.checkboxes}>
+        <AdminCheckBox labelText="В наявності" name="available" register={register} />
+        <AdminCheckBox labelText="Новинка" name="is_new" register={register} />
+        <AdminCheckBox labelText="Хіт продажу" name="is_best_seller" register={register} />
+      </div>
+      <div className={cl.productNumericalInputs}>
+        <Input
+          placeholder="0"
+          type="number"
+          labelText="Об’єм(ml):"
+          name="volume_ml"
+          register={register}
+          errors={errors}
+          variant="admin"
+        />
+        <Input
+          placeholder="0"
+          variant="admin"
+          type="number"
+          labelText="Ціна (грн):"
+          name="price"
+          register={register}
+          errors={errors}
+        />
+        <Input
+          placeholder="0"
+          variant="admin"
+          type="number"
+          labelText="Знижка(%):"
+          name="discount"
+          register={register}
+          errors={errors}
+        />
+        <Input
+          placeholder="0"
+          variant="admin"
+          type="number"
+          labelText="Ціна зі знижкою:"
+          name="discounted_price"
+          register={register}
+          errors={errors}
+          readOnly
+        />
+      </div>
+      <div className={cl.selects}>
+        <CategorySelect control={control} errors={errors} />
+        <TypeSelect control={control} errors={errors} />
+      </div>
 
       <ResponsiveTextareas register={register} errors={errors} control={control} />
 
