89315c6f23ab1b97d8ec7aa3c3542feba8190806
Fresh Lemur <dimalimaredmi@gmail.com>
2025-07-08 14:05:56 +0300

    feature: implemented logic for delete review

--- client/src/components/admin/ReviewsManagement/ReviewsList/ReviewCard/ReviewCard.jsx
+++ client/src/components/admin/ReviewsManagement/ReviewsList/ReviewCard/ReviewCard.jsx
@@ -2,43 +2,60 @@ import { useDispatch } from 'react-redux';
 import cl from './index.module.scss';
 import { approveReview, deleteReview } from '@redux/reviews/service';
 import Button from '@components/UI/Button/Button';
+import { useState } from 'react';
+import DeletePopUp from '@components/admin/DeletePopUp/DeletePopUp';
 
-const ReviewCard = ({ review }) => {
+const ReviewCard = ({ review, showDeletedMessage }) => {
+  const [isShowDeletePopUp, setIsShowDeletePopUp] = useState(false);
   const dispatch = useDispatch();
-  const handleDelete = (productId, reviewId) => dispatch(deleteReview({ productId, reviewId }));
+  const handleDelete = (productId, reviewId) => {
+    dispatch(deleteReview({ productId, reviewId }));
+    showDeletedMessage(`Відгук успішно видалено`);
+  };
   const handleApprove = (productId, reviewId) => dispatch(approveReview({ productId, reviewId }));
+  const toggleDeletePopUp = () => setIsShowDeletePopUp(!isShowDeletePopUp);
 
   return (
-    <li className={cl.reviewCard}>
-      <p className={review.is_approved ? cl.approveed : cl.notApproved}>
-        {review.is_approved ? 'Опубліковано' : 'Не опубліковано'}
-      </p>
-      <p>{review.review_text}</p>
-      <div className={cl.reviewDetailWrap}>
-        <p className={cl.reviewDetail}>
-          Оцінка: <span>{review.rating}</span>
+    <>
+      <li className={cl.reviewCard}>
+        <p className={review.is_approved ? cl.approveed : cl.notApproved}>
+          {review.is_approved ? 'Опубліковано' : 'Не опубліковано'}
         </p>
+        <p>{review.review_text}</p>
+        <div className={cl.reviewDetailWrap}>
+          <p className={cl.reviewDetail}>
+            Оцінка: <span>{review.rating}</span>
+          </p>
+          <p className={cl.reviewDetail}>
+            Дата: <span>{review.date}</span>
+          </p>
+        </div>
         <p className={cl.reviewDetail}>
-          Дата: <span>{review.date}</span>
+          Ім`я користувача: <span>{review.user_name}</span>
         </p>
-      </div>
-      <p className={cl.reviewDetail}>
-        Ім`я користувача: <span>{review.user_name}</span>
-      </p>
-      {review.is_approved ? (
-        <button>
-          <img src="/svg/admin/delete.svg" alt="More options" />
-        </button>
-      ) : (
-        <div className={cl.approveButtonsWrap}>
-          <Button onClick={() => handleApprove(review.product_id, review.id)}>Опублікувати</Button>
-          <button>
+        {review.is_approved ? (
+          <button onClick={toggleDeletePopUp}>
             <img src="/svg/admin/delete.svg" alt="More options" />
           </button>
-          {/* <button onClick={() => handleDelete(review.product_id, review.id)}>Видалити</button> */}
-        </div>
+        ) : (
+          <div className={cl.approveButtonsWrap}>
+            <Button onClick={() => handleApprove(review.product_id, review.id)}>Опублікувати</Button>
+            <button>
+              <img src="/svg/admin/delete.svg" alt="More options" />
+            </button>
+          </div>
+        )}
+      </li>
+
+      {isShowDeletePopUp && (
+        <DeletePopUp
+          closeDeletePopUp={toggleDeletePopUp}
+          handleDelete={() => handleDelete(review.product_id, review.id)}
+        >
+          Ви впевнені, що хочете видалити цей відгук?
+        </DeletePopUp>
       )}
-    </li>
+    </>
   );
 };
 export default ReviewCard;
--- client/src/components/admin/ReviewsManagement/ReviewsList/ReviewsList.jsx
+++ client/src/components/admin/ReviewsManagement/ReviewsList/ReviewsList.jsx
@@ -3,19 +3,25 @@ import { loadReviews, selectReviews } from '@redux/selectors';
 import { useSelector } from 'react-redux';
 import cl from './index.module.scss';
 import ReviewCard from './ReviewCard/ReviewCard';
+import useTimedMessage from '@hooks/admin/useTimedMessage';
+import AdminMessage from '@components/admin/AdminMessage/AdminMessage';
 
 const ReviewsList = () => {
   const reviews = useSelector(selectReviews);
   const isLoading = useSelector(loadReviews);
+  const [deletedMessage, showDeletedMessage] = useTimedMessage();
 
   return isLoading ? (
     <Spinner />
   ) : reviews.length > 0 ? (
-    <ul className={cl.reviewsList}>
-      {reviews.map((review) => (
-        <ReviewCard review={review} key={review.id} />
-      ))}
-    </ul>
+    <>
+      <ul className={cl.reviewsList}>
+        {reviews.map((review) => (
+          <ReviewCard review={review} key={review.id} showDeletedMessage={showDeletedMessage} />
+        ))}
+      </ul>
+      {deletedMessage && <AdminMessage>{deletedMessage}</AdminMessage>}
+    </>
   ) : (
     <p className={cl.notFoundText}>Нічого не знайдено. Плак плак.</p>
   );
