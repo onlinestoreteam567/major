28077931f4a62568337ffaccdb3a39ddc8f5318b
Fresh Lemur <dimalimaredmi@gmail.com>
2025-07-15 12:59:40 +0300

    fix: implemented error for image placeholder in categories

--- client/src/components/admin/ImageUpload/ImagePreview/ImagePreview.jsx
+++ client/src/components/admin/ImageUpload/ImagePreview/ImagePreview.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import cl from './index.module.scss';
 
-const ImagePreview = ({ file, uploadedImage }) => {
+const ImagePreview = ({ file, uploadedImage, errors, name }) => {
   const [previewUrl, setPreviewUrl] = useState('/images/admin/categoryPlaceholder.webp');
 
   useEffect(() => {
@@ -21,7 +21,7 @@ const ImagePreview = ({ file, uploadedImage }) => {
   if (!previewUrl) return null;
 
   return (
-    <div className={cl.previewImage}>
+    <div className={`${cl.previewImage} ${errors && errors[name] && cl.error}`}>
       <img src={previewUrl} alt="Image Preview" />
     </div>
   );
