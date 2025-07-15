34341765573ad8e70770422741efed04aa6ea06d
Fresh Lemur <dimalimaredmi@gmail.com>
2025-07-15 13:38:47 +0300

    feature: add checkout page localization and breadcrumbs component

---
+++ client/src/pages/CheckoutPage/CheckoutBreadCrumbs/CheckoutBreadCrumbs.jsx
@@ -0,0 +1,14 @@
+import cl from './index.module.scss';
+import useTranslationNamespace from '@hooks/useTranslationNamespace';
+
+const CheckoutBreadCrumbs = () => {
+  const { getTranslation } = useTranslationNamespace('checkoutPage');
+
+  return (
+    <button className={cl.checkoutBreadCrumbs}>
+      <img src="/svg/banners/arrowLeft.svg" alt="" />
+      {getTranslation('continueShopping')}
+    </button>
+  );
+};
+export default CheckoutBreadCrumbs;
--- client/src/pages/CheckoutPage/CheckoutPage.jsx
+++ client/src/pages/CheckoutPage/CheckoutPage.jsx
@@ -1,11 +1,17 @@
+import Heading from '@components/UI/Texts/Heading/Heading';
 import Cart from './Cart/Cart';
+import CheckoutBreadCrumbs from './CheckoutBreadCrumbs/CheckoutBreadCrumbs';
 import cl from './index.module.scss';
 import NovaPost from './NovaPost/NovaPost';
 import Promocode from './Promocode/Promocode';
-
+import useTranslationNamespace from '@hooks/useTranslationNamespace';
 const CheckoutPage = () => {
+  const { getTranslation } = useTranslationNamespace('checkoutPage');
+
   return (
     <div className={cl.checkoutPage}>
+      <CheckoutBreadCrumbs />
+      <Heading type="h2">{getTranslation('checkout')}</Heading>
       <NovaPost />
       <Cart />
       <Promocode />
