5189733dc3135e2635ba5d22b8f6e2d3d3a0a7bd
Fresh Lemur <dimalimaredmi@gmail.com>
2025-07-16 11:25:04 +0300

    feature: enhance checkout page with item count display and improved cart item layout

--- client/src/pages/CheckoutPage/Cart/Cart.jsx
+++ client/src/pages/CheckoutPage/Cart/Cart.jsx
@@ -1,26 +1,43 @@
 import { useSelector } from 'react-redux';
 import cl from './index.module.scss';
-import CartItem from './CartItem/CartItem';
 import { selectPromocode } from '@redux/selectors';
 import { useEffect, useState } from 'react';
 import calculateDiscountedItems from './helpers/calculateDiscountedItems';
+import Heading from '@components/UI/Texts/Heading/Heading';
+import useTranslationNamespace from '@hooks/useTranslationNamespace';
+import Arrow from '@assets/svg/Admin/Arrow/Arrow';
+import CheckoutCartItem from './CartItem/CartItem';
 
 const Cart = () => {
+  const { getTranslation } = useTranslationNamespace('checkoutPage');
   const cartItems = useSelector((state) => state.cart.items);
   const promocodeDiscount = useSelector(selectPromocode);
   const [discountedItems, setDiscountedItems] = useState([...cartItems]);
+  const [isExpanded, setIsExpanded] = useState(true);
+
+  const toggleExpanded = () => setIsExpanded(!isExpanded);
 
   useEffect(() => {
     setDiscountedItems(calculateDiscountedItems(promocodeDiscount, cartItems));
   }, [promocodeDiscount, cartItems]);
 
   return (
-    <div className={cl.cart}>
-      <ul>
-        {discountedItems.map((product) => (
-          <CartItem key={product.id} product={product} />
-        ))}
-      </ul>
+    <div className={cl.checkoutCart}>
+      <div className={isExpanded ? cl.expanded : ''}>
+        <Heading type="h4">
+          {getTranslation('NumberOfItemsInTheCart')} {discountedItems.length}
+        </Heading>
+        <button onClick={toggleExpanded}>
+          <Arrow />
+        </button>
+      </div>
+      {isExpanded && (
+        <ul>
+          {discountedItems.map((product) => (
+            <CheckoutCartItem key={product.id} product={product} />
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
--- client/src/pages/CheckoutPage/Cart/CartItem/CartItem.jsx
+++ client/src/pages/CheckoutPage/Cart/CartItem/CartItem.jsx
@@ -1,25 +1,41 @@
 import Heading from '@components/UI/Texts/Heading/Heading';
 import cl from './index.module.scss';
+import Paragraph from '@components/UI/Texts/Paragraph/Paragraph';
+import ButtonClose from '@components/UI/Button/ButtonClose/ButtonClose';
+import { removeItem } from '@features/cart/cartSlice';
+import { useDispatch } from 'react-redux';
 
-const CartItem = ({ product }) => {
+const CheckoutCartItem = ({ product }) => {
+  const dispatch = useDispatch();
   const hryvnia = '\u20B4';
-  console.log(product);
+
+  const handleRemoveItem = () => dispatch(removeItem(product.id));
+
   return (
     <li className={cl.cartItem}>
-      <img src={product.images[0].image} className={cl.basketItemImg} alt={product.name} />
-      <Heading type="h4">{product.name}</Heading>
-      <div className={cl.priceWrap}>
-        <Heading type="h4">
-          {product.price_with_discount || product.price} {hryvnia}
-        </Heading>
-        {product.is_discount && (
-          <div className={cl.oldPrice}>
-            <span className={cl.costOld}>{product.price}</span>
-            <span className={cl.hrnGrey}>{hryvnia}</span>
-          </div>
-        )}
-      </div>
+      <img src={product.images[0].image} alt={product.name} />
+
+      <section>
+        <div>
+          <Paragraph type="body2">{product.name}</Paragraph>
+          <ButtonClose onClick={handleRemoveItem} />
+        </div>
+
+        <div>
+          <Heading type="h4">
+            <span>Ціна</span>
+            {product.price_with_discount || product.price} {hryvnia}
+          </Heading>
+          <Heading type="h4">
+            <span>Кількість</span>х {product.quantity}
+          </Heading>
+          <Heading type="h4">
+            <span>Сума</span>
+            {product.price_with_discount || product.price * product.quantity} {hryvnia}
+          </Heading>
+        </div>
+      </section>
     </li>
   );
 };
-export default CartItem;
+export default CheckoutCartItem;
