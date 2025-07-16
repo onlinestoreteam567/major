99cfd0edd1d801d880c966f00eee7d7808a1627f
Fresh Lemur <dimalimaredmi@gmail.com>
2025-07-16 12:48:34 +0300

    feature: implemented top section of promocode management, add PromocodeSelect and PromocodeSelectOptions components with styles

--- client/src/components/admin/PromocodeManagement/PromocodeManagement.jsx
+++ client/src/components/admin/PromocodeManagement/PromocodeManagement.jsx
@@ -1,15 +1,17 @@
 import { Link } from 'react-router-dom';
-import ReturnButton from '../ReturnButton/ReturnButton';
 import cl from './index.module.scss';
 import List from './PromocodeList/PromocodeList.jsx';
+import PromocodeSelect from './PromocodeSelect/PromocodeSelect';
 
 const PromocodeManagement = () => {
   return (
     <div className={cl.promocodeManagement}>
       <div>
-        <Link to={`/admin/promocodes/create`}>+</Link>
-        <ReturnButton />;
+        <p>Промокоди (30)</p>
+        <Link to={`/admin/promocodes/create`}>Створити промо</Link>
+        <PromocodeSelect />
       </div>
+
       <List />
     </div>
   );
