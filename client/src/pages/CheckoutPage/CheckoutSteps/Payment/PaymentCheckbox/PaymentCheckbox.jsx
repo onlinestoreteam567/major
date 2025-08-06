b1bc595598593eecae238d5ad299bccdf3310973
Fresh Lemur <dimalimaredmi@gmail.com>
2025-08-06 08:36:05 +0300

    feature: implemented error checkbox

--- client/src/pages/CheckoutPage/CheckoutSteps/Payment/PaymentCheckbox/PaymentCheckbox.jsx
+++ client/src/pages/CheckoutPage/CheckoutSteps/Payment/PaymentCheckbox/PaymentCheckbox.jsx
@@ -5,9 +5,11 @@ import useTranslationNamespace from '@hooks/useTranslationNamespace';
 export default function PaymentCheckbox({ name, register, errors, ...rest }) {
   const { getTranslation } = useTranslationNamespace('checkoutPage');
 
+  console.log(errors?.[name]);
+
   return (
     <label htmlFor={name} className={cl.paymentCheckbox}>
-      <div>
+      <div className={errors?.[name] ? cl.error : ''}>
         <input id={name} className={cl.checkbox} type="checkbox" {...register(name)} {...rest} defaultChecked />
         <span />
         {getTranslation('paymentCheckbox')}
