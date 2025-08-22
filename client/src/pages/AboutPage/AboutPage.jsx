883b2cc5f360aba78faf6ddc8cb1af5621cdd84d
Fresh Lemur <dimalimaredmi@gmail.com>
2025-08-22 13:43:06 +0300

    feature: implemented second section of the about page, with placeholder image

--- client/src/pages/AboutPage/AboutPage.jsx
+++ client/src/pages/AboutPage/AboutPage.jsx
@@ -1,5 +1,6 @@
 import Heading from '@components/UI/Texts/Heading/Heading';
 import cl from './index.module.scss';
+import Paragraph from '@components/UI/Texts/Paragraph/Paragraph';
 
 const AboutPage = () => {
   return (
@@ -12,6 +13,31 @@ const AboutPage = () => {
           </Heading>
         </figcaption>
       </figure>
+      <section>
+        <Heading type="h2">Major — це більше, ніж просто бізнес</Heading>
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <img src="/images/placeholder.webp" alt="" />
+        </div>
+        <div>
+          <Paragraph type="body1">
+            Це сімейна справа, яка об’єднує всіх нас у спільному прагненні створювати дійсно дієві продукти.
+          </Paragraph>
+          <Paragraph type="body1">
+            У нас невелика але потужна команда, де кожен відповідає за свій процес: від створення формул і тестування до
+            пакування і консультації клієнтів.
+          </Paragraph>
+          <Paragraph type="body1">
+            Для нас українське — це не тренд і не гасло.
+            <br />
+            Це справа життя. Ми його створюємо власноруч. І водночас - захищаємо. <br />
+            Бо любов — це не лише слова, це дії.
+          </Paragraph>
+        </div>
+      </section>
     </div>
   );
 };
