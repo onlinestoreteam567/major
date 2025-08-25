761049662f091e504f73ece4005a690bec07503e
Fresh Lemur <dimalimaredmi@gmail.com>
2025-08-25 15:13:04 +0300

    feature: implemented history section of about us page

--- client/src/pages/AboutPage/AboutPage.jsx
+++ client/src/pages/AboutPage/AboutPage.jsx
@@ -38,6 +38,22 @@ const AboutPage = () => {
           </Paragraph>
         </div>
       </section>
+      <section>
+        <Heading type="h2">Коли світ зупинився — ми почали</Heading>
+        {/* TODO fill alts */}
+        <img src="/images/about/pandemic.webp" alt="" />
+        <div>
+          <Paragraph type="body1">
+            У 2019 році в період пандемії світ зупинився, двері салонів зачинилися, але бажання жінок піклуватися про
+            себе нікуди не зникло.
+          </Paragraph>
+          <Paragraph type="body1">
+            Тоді ми вирішили: <br />
+            &quot;Якщо неможливо потрапити до салону — ми створимо салон вдома.&quot; Саме тоді зародилася ідея
+            створювати продукти, які забеспечать салонний ефект вдома.
+          </Paragraph>
+        </div>
+      </section>
     </div>
   );
 };
