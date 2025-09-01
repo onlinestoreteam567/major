840c394aeb70ee7bfb039d70c8532e04ff415f2f
Fresh Lemur <dimalimaredmi@gmail.com>
2025-09-01 10:56:40 +0300

    fix: remove unused defaultSettings from settingSlider.js

--- client/src/components/constants/settingSlider.js
+++ client/src/components/constants/settingSlider.js
@@ -27,41 +27,6 @@ export const commentsSettings = {
   ],
 };
 
-export const defaultSettings = {
-  accessibility: true,
-  dots: false,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  lazyLoad: true,
-  fade: true,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        infinite: true,
-      },
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-      },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-  ],
-};
-
 export const mainSettings = {
   accessibility: true,
   dots: false,
