732e124b1aa39f4363577619be8ed523864a8443
Fresh Lemur <dimalimaredmi@gmail.com>
2025-09-04 08:33:47 +0300

    fix: update navigation for admin routes to redirect to products page

--- client/src/router/layouts/ProtectedRoute/ProtectedRoute.jsx
+++ client/src/router/layouts/ProtectedRoute/ProtectedRoute.jsx
@@ -1,12 +1,21 @@
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useLocation, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectAccessToken } from '../../../redux/selectors';
 import AdminNavigation from '../../../components/admin/AdminNavigation/AdminNavigation';
 import cl from './index.module.scss';
 import AdminGlobalMessage from './AdminGlobalMessage';
+import { useEffect } from 'react';
 
 const ProtectedRoute = () => {
   const auth = useSelector(selectAccessToken);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === '/admin') {
+      navigate('/admin/products');
+    }
+  }, [location]);
 
   return auth ? (
     <div className={cl.adminLayout}>
