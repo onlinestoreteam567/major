bffbbcb2305fc1fba4c558cd962917323d10a899
Fresh Lemur <dimalimaredmi@gmail.com>
2025-09-10 09:52:30 +0300

    feat: refactor footer components to fetch and display dynamic contact information

--- client/src/components/Footer/FooterCopyrightSection/FooterCopyrightSection.jsx
+++ client/src/components/Footer/FooterCopyrightSection/FooterCopyrightSection.jsx
@@ -1,12 +1,18 @@
 import cl from './index.module.scss';
 import useTranslationNamespace from '@hooks/useTranslationNamespace';
 
-const FooterCopyrightSection = () => {
+const FooterCopyrightSection = ({ contacts }) => {
   const { getTranslation } = useTranslationNamespace('footer');
+  if (!contacts) return;
+
+  const year = `${contacts[0].current_year}-${contacts[0].current_year + 1}`;
+
   return (
     <section className={cl.footerCopyrightSection}>
       <hr />
-      <h3>{getTranslation('copyright')}</h3>
+      <h3>
+        {getTranslation('copyright1')} {year}, {getTranslation('copyright2')}
+      </h3>
     </section>
   );
 };
--- client/src/components/Footer/FooterTopSection/FooterInfo/FooterInfo.jsx
+++ client/src/components/Footer/FooterTopSection/FooterInfo/FooterInfo.jsx
@@ -1,19 +1,20 @@
 import cl from './index.module.scss';
 import useTranslationNamespace from '@hooks/useTranslationNamespace';
 
-const FooterInfo = () => {
+const FooterInfo = ({ contacts }) => {
   const { getTranslation } = useTranslationNamespace('footer');
 
+  if (!contacts) return;
   return (
     <section className={cl.footerInfo}>
       <div className={`${cl.workingHoursWrapper} ${cl.wrapper}`}>
         <ul className={cl.workingHours}>
           <li className={cl.liWithImg}>
             <img src="/svg/footer/calendar.svg" alt={getTranslation('calendarAlt')} />
-            <h3>{getTranslation('monFri')}</h3>
+            <h3>{contacts[0].work_schedule_weekdays}</h3>
           </li>
           <li>
-            <h3>{getTranslation('satSun')}</h3>
+            <h3>{contacts[0].work_schedule_weekdays}</h3>
           </li>
         </ul>
       </div>
@@ -22,10 +23,10 @@ const FooterInfo = () => {
         <ul>
           <li className={cl.liWithImg}>
             <img className={cl.phoneCallIcon} src="/svg/footer/phoneCall.svg" alt={getTranslation('phoneAlt')} />
-            <h3 className={cl.titleWithIcon}>+38 (096) 327 77 34</h3>
+            <h3 className={cl.titleWithIcon}>{contacts[0].main_phone_number}</h3>
           </li>
           <li>
-            <h3>+38 (050) 123 84 99</h3>
+            <h3>{contacts[0].additional_phone_number}</h3>
           </li>
         </ul>
       </div>
