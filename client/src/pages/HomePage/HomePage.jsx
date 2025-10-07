aeaf35a034192778371e375e0244c7181d39be9f
Fresh Lemur <dimalimaredmi@gmail.com>
2025-10-07 10:33:42 +0300

    feature: implemented static meta tags into index.html

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -8,27 +8,10 @@ import YellowButton from './YellowButton/YellowButton';
 import BestSellers from './MainSliders/BestSellers';
 import Sets from './MainSliders/Sets';
 import OurPartners from './OurPartners/OurPartners';
-import { Helmet } from 'react-helmet-async';
-
-const siteName = 'Major';
-const siteDescription = 'Дуже крутий опис для сайту';
-const siteImage = 'https://major-gamma.vercel.app/svg/favicon.svg';
 
 const HomePage = () => {
   return (
     <div className={cl.homePage}>
-      {/* <Helmet>
-        <title>Site name</title>
-        <meta property="og:title" content="Site name" />
-        <meta property="og:description" content="site desc" />
-        <meta property="og:image" content="https://major-gamma.vercel.app/svg/favicon.svg" />
-        <meta property="og:url" content="https://major-gamma.vercel.app/" />
-        <meta property="og:type" content="website" />
-        <meta name="twitter:card" content="https://major-gamma.vercel.app/svg/favicon.svg" />
-        <meta name="twitter:title" content={siteName} />
-        <meta name="twitter:description" content={siteDescription} />
-        <meta name="twitter:image" content="https://major-gamma.vercel.app/svg/favicon.svg" />
-      </Helmet> */}
       <div className={cl.banner}>
         <MainBanner />
       </div>
