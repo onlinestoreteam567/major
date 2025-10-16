c52fab072a0b86d0f47ac2e4fd8d0071e635973f
Fresh Lemur <dimalimaredmi@gmail.com>
2025-10-16 14:41:21 +0300

    feature: implemented lazy imports for routes in AppRouter to improve app performance

--- client/src/router/AppRouter.jsx
+++ client/src/router/AppRouter.jsx
@@ -3,39 +3,15 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import MainLayout from './layouts/MainLayout/MainLayout';
 
-// Error Boundary
-import ErrorBoundary from './error/ErrorBoundary'; // A component to handle errors
+import ErrorBoundary from './error/ErrorBoundary';
 import NotFound from '@pages/NotFoundPage/NotFoundPage';
 import ErrorPage from '@pages/ErrorPage/ErrorPage';
 import AppLoader from '@router/AppLoader/AppLoader';
 
-// Protected Route (Admin)
 import ProtectedRoute from './layouts/ProtectedRoute/ProtectedRoute';
-import AdminLoginPage from '@components/admin/AdminLoginPage/AdminLoginPage';
-import ProductManagement from '@components/admin/ProductManagement/ProductManagement';
-import ProductCreate from '@components/admin/ProductManagement/ProductCreate/ProductCreate';
-import ProductEdit from '@components/admin/ProductManagement/ProductEdit/ProductEdit';
-import CategoriesManagement from '@components/admin/CategoriesManagement/CategoriesManagement';
-import CategoryCreate from '@components/admin/CategoriesManagement/CategoryCreate/CategoryCreate';
-import PurposeEdit from '@components/admin/CategoriesManagement/Purpose/PurposeEdit/PurposeEdit';
-import TypeEdit from '@components/admin/CategoriesManagement/Type/TypeEdit/TypeEdit';
-import PromocodeManagement from '@components/admin/PromocodeManagement/PromocodeManagement';
-import PromocodeCreate from '@components/admin/PromocodeManagement/PromocodeCreate/PromocodeCreate';
-import PromocodeEdit from '@components/admin/PromocodeManagement/PromocodeEdit/PromocodeEdit';
-import BannerManagement from '@components/admin/BannerManagement/BannerManagement';
-import BannerCreate from '@components/admin/BannerManagement/BannerCreate/BannerCreate';
-import BannerEdit from '@components/admin/BannerManagement/BannerEdit/BannerEdit';
-import PartnersManagement from '@components/admin/PartnersManagement/PartnersManagement';
-import PartnerCreate from '@components/admin/PartnersManagement/PartnerCreate/PartnerCreate';
-import PartnerEdit from '@components/admin/PartnersManagement/PartnerEdit/PartnerEdit';
-import ReviewsManagement from '@components/admin/ReviewsManagement/ReviewsManagement';
-import ContactsManagement from '@components/admin/ContactsManagement/ContactsManagement';
 import RoleBasedRoute from './RoleBasedRoute';
-import Settings from '@components/admin/Settings/Settings';
-import ManagerCreate from '@components/admin/Settings/ManagerCreate/ManagerCreate';
-import ManagerEdit from '@components/admin/Settings/ManagerEdit/ManagerEdit';
 
-// Lazy loading
+// --- PUBLIC PAGES  ---
 const Home = lazy(() => import('@pages/HomePage/HomePage'));
 const Catalog = lazy(() => import('@pages/CatalogPage/CatalogPage'));
 const About = lazy(() => import('@pages/AboutPage/AboutPage'));
@@ -44,9 +20,36 @@ const Cooperation = lazy(() => import('@pages/СooperationPage'));
 const ProductPage = lazy(() => import('@pages/ProductPage/ProductPage'));
 const CheckoutPage = lazy(() => import('@pages/CheckoutPage/CheckoutPage'));
 
-// Example for future features
-// const Login = lazy(() => import('@pages/Login'));
-// const Register = lazy(() => import('@pages/Register'));
+// --- ADMIN PAGES ---
+const AdminLoginPage = lazy(() => import('@components/admin/AdminLoginPage/AdminLoginPage'));
+
+const ProductManagement = lazy(() => import('@components/admin/ProductManagement/ProductManagement'));
+const ProductCreate = lazy(() => import('@components/admin/ProductManagement/ProductCreate/ProductCreate'));
+const ProductEdit = lazy(() => import('@components/admin/ProductManagement/ProductEdit/ProductEdit'));
+
+const CategoriesManagement = lazy(() => import('@components/admin/CategoriesManagement/CategoriesManagement'));
+const CategoryCreate = lazy(() => import('@components/admin/CategoriesManagement/CategoryCreate/CategoryCreate'));
+const PurposeEdit = lazy(() => import('@components/admin/CategoriesManagement/Purpose/PurposeEdit/PurposeEdit'));
+const TypeEdit = lazy(() => import('@components/admin/CategoriesManagement/Type/TypeEdit/TypeEdit'));
+
+const PromocodeManagement = lazy(() => import('@components/admin/PromocodeManagement/PromocodeManagement'));
+const PromocodeCreate = lazy(() => import('@components/admin/PromocodeManagement/PromocodeCreate/PromocodeCreate'));
+const PromocodeEdit = lazy(() => import('@components/admin/PromocodeManagement/PromocodeEdit/PromocodeEdit'));
+
+const ReviewsManagement = lazy(() => import('@components/admin/ReviewsManagement/ReviewsManagement'));
+const ContactsManagement = lazy(() => import('@components/admin/ContactsManagement/ContactsManagement'));
+
+const Settings = lazy(() => import('@components/admin/Settings/Settings'));
+const ManagerCreate = lazy(() => import('@components/admin/Settings/ManagerCreate/ManagerCreate'));
+const ManagerEdit = lazy(() => import('@components/admin/Settings/ManagerEdit/ManagerEdit'));
+
+const BannerManagement = lazy(() => import('@components/admin/BannerManagement/BannerManagement'));
+const BannerCreate = lazy(() => import('@components/admin/BannerManagement/BannerCreate/BannerCreate'));
+const BannerEdit = lazy(() => import('@components/admin/BannerManagement/BannerEdit/BannerEdit'));
+
+const PartnersManagement = lazy(() => import('@components/admin/PartnersManagement/PartnersManagement'));
+const PartnerCreate = lazy(() => import('@components/admin/PartnersManagement/PartnerCreate/PartnerCreate'));
+const PartnerEdit = lazy(() => import('@components/admin/PartnersManagement/PartnerEdit/PartnerEdit'));
 
 const router = createBrowserRouter([
   {
@@ -90,10 +93,13 @@ const router = createBrowserRouter([
           { path: 'banners', element: <BannerManagement /> },
           { path: 'banners/create', element: <BannerCreate /> },
           { path: 'banners/:id', element: <BannerEdit /> },
+
           { path: 'partners', element: <PartnersManagement /> },
           { path: 'partners/create', element: <PartnerCreate /> },
           { path: 'partners/:id', element: <PartnerEdit /> },
+
           { path: 'contacts', element: <ContactsManagement /> },
+
           { path: 'settings', element: <Settings /> },
           { path: 'settings/create', element: <ManagerCreate /> },
           { path: 'settings/:id', element: <ManagerEdit /> },
@@ -102,16 +108,16 @@ const router = createBrowserRouter([
     ],
   },
   { path: '/admin/login', element: <AdminLoginPage /> },
+
   {
     path: '*',
     element: <NotFound />,
   },
-  /*** */
+
   {
     path: '/error-test',
     element: <ErrorPage />,
   },
-  /**** */
 ]);
 
 const AppRouter = () => {
