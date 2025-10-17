12ed65a0f3a5f231759bc665998af2ec38fef4d8
Fresh Lemur <dimalimaredmi@gmail.com>
2025-10-17 11:22:24 +0300

    feature: implemented code splitting for redux reducers

--- client/src/config/store.js
+++ client/src/config/store.js
@@ -1,61 +1,32 @@
+// store.js
+
+// Static Reducer Imports (These are bundled with the main application)
 import { cartReducer } from '@redux/cart/cartSlice';
-import { bannerByIdReducer } from '@redux/banner/bannerByIdSlice';
-import { bannerCreateReducer } from '@redux/banner/bannerCreateSlice';
-import { bannerDeleteReducer } from '@redux/banner/bannerDeleteSlice';
-import { bannerEditReducer } from '@redux/banner/bannerEditSlice';
-import { bannerReducer } from '@redux/banner/bannerSlice';
-import { filterReducer } from '@redux/filter/filterSlice';
-import { settlementsReducer } from '@redux/novaPost/settlementsSlice';
-import { warehousesReducer } from '@redux/novaPost/warehousesSlice';
+import { setsReducer } from '@redux/products/setsSlice';
+import { productsReducer } from '@redux/products/listSlice';
+import { productIdReducer } from '@redux/products/cardSlice';
+import { bestSellerReducer } from '@redux/products/bestSlice';
 import { categotyReducer } from '@redux/params/categorySlice';
 import { typesReducer } from '@redux/params/purposeSlice';
-import { partnerCreateReducer } from '@redux/partners/partnerCreateSlice';
-import { partnerDeleteReducer } from '@redux/partners/partnerDeleteSlice';
-import { partnersReducer } from '@redux/partners/partnerSlice';
-import { bestSellerReducer } from '@redux/products/bestSlice';
-import { productIdReducer } from '@redux/products/cardSlice';
 import { fitCategoryReducer } from '@redux/products/fitCategorySlice';
-import { productsReducer } from '@redux/products/listSlice';
+import { filterReducer } from '@redux/filter/filterSlice';
 import { searchReducer } from '@redux/products/searchSlice';
-import { setsReducer } from '@redux/products/setsSlice';
-import { viewedProductsReducer } from '@redux/products/viewedProductsSlice';
 import { promocodeReducer } from '@redux/promocode/promocodeSlice';
+import { bannerReducer } from '@redux/banner/bannerSlice';
+import { viewedProductsReducer } from '@redux/products/viewedProductsSlice';
+import { settlementsReducer } from '@redux/novaPost/settlementsSlice';
+import { warehousesReducer } from '@redux/novaPost/warehousesSlice';
+import { partnersReducer } from '@redux/partners/partnerSlice';
+import { authReducer } from '@redux/admin/auth/authSlice';
+import { verifyTokenReducer } from '@redux/admin/auth/verifyTokenSlice';
+import { contactsReducer } from '@redux/contacts/contactsSlice';
+import { reviewReducer } from '@redux/reviews/reviewsSlice';
+
+// Core Redux Toolkit and Utilities
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { loadFromStorage, saveToStorage } from '@utils/localStorage';
-import { authReducer } from '../redux/admin/auth/authSlice';
-import { createProductReducer } from '../redux/admin/product/createProductSlice';
-import { createPurposeCategoryReducer } from '../redux/admin/purpose/createPurposeCategorySlice';
-import { createTypeCategoryReducer } from '../redux/admin/type/createTypeCategorySlice';
-import { editProductReducer } from '../redux/admin/product/editProductSlice';
-import { productDeleteReducer } from '../redux/admin/product/productDeleteSlice';
-import { promocodeByIdReducer } from '../redux/admin/promocode/promocodeByIdSlice';
-import { promocodeCreateReducer } from '../redux/admin/promocode/promocodeCreateSlice';
-import { promocodeDeleteReducer } from '../redux/admin/promocode/promocodeDeleteSlice';
-import { promocodeEditReducer } from '../redux/admin/promocode/promocodeEditSlice';
-import { promocodeListReducer } from '../redux/admin/promocode/promocodeListSlice';
-import { purposeCategoryByIdReducer } from '../redux/admin/purpose/purposeCategoryByIdSlice';
-import { purposeDeleteReducer } from '../redux/admin/purpose/purposeDeleteSlice';
-import { purposeEditReducer } from '../redux/admin/purpose/purposeEditSlice';
-import { typeByIdReducer } from '../redux/admin/type/typeByIdSlice';
-import { typeDeleteReducer } from '../redux/admin/type/typeDeleteSlice';
-import { typeEditReducer } from '../redux/admin/type/typeEditSlice';
-import { reviewReducer } from '../redux/reviews/reviewsSlice';
-import { uploadImageReducer } from '@redux/blogs/uploadImageSlice';
-import { partnerByIdReducer } from '@redux/partners/partnerByIdSlice';
-import { partnerEditReducer } from '@redux/partners/partnerEditSlice';
-import { addReviewReducer } from '@redux/reviews/addReviewSlice';
-import { adminProductSearchReducer } from '@redux/admin/search/adminProductSearchSlice';
-import { adminReviewsSearchReducer } from '@redux/admin/search/adminReviewsSearchSlice';
-import { adminMessageSliceReducer } from '@redux/admin/adminMessageSlice';
-import { verifyTokenReducer } from '@redux/admin/auth/verifyTokenSlice';
-import { contactsEditReducer } from '@redux/admin/contacts/contactsEditSlice';
-import { contactsReducer } from '@redux/contacts/contactsSlice';
-import { usersListReducer } from '@redux/admin/settings/userList';
-import { userDeleteReducer } from '@redux/admin/settings/userDelete';
-import { userCreateReducer } from '@redux/admin/settings/userCreate';
-import { userEditReducer } from '@redux/admin/settings/userEdit';
-import { userByIdReducer } from '@redux/admin/settings/userById';
 
+// Initial state loaded from local storage
 const preloadedState = {
   cart: loadFromStorage('cart') || undefined,
   auth: {
@@ -68,6 +39,7 @@ const preloadedState = {
   },
 };
 
+// Define the reducers that will always be present
 const staticReducers = {
   cart: cartReducer,
   sets: setsReducer,
@@ -88,54 +60,51 @@ const staticReducers = {
   auth: authReducer,
   verifyToken: verifyTokenReducer,
   contacts: contactsReducer,
+  reviews: reviewReducer,
 };
 
-const rootReducer = combineReducers({
-  uploadImage: uploadImageReducer,
-  partnerById: partnerByIdReducer,
-  partnerEdit: partnerEditReducer,
-  partnerDelete: partnerDeleteReducer,
-  partnerCreate: partnerCreateReducer,
-  bannerDelete: bannerDeleteReducer,
-  bannerCreate: bannerCreateReducer,
-  bannerById: bannerByIdReducer,
-  bannerEdit: bannerEditReducer,
-  adminMessage: adminMessageSliceReducer,
-  createProduct: createProductReducer,
-  editProduct: editProductReducer,
-  productDelete: productDeleteReducer,
-  createPurposeCategory: createPurposeCategoryReducer,
-  createTypeCategory: createTypeCategoryReducer,
-  purposeCategoryById: purposeCategoryByIdReducer,
-  purposeEdit: purposeEditReducer,
-  purposeDelete: purposeDeleteReducer,
-  typeById: typeByIdReducer,
-  typeEdit: typeEditReducer,
-  typeDelete: typeDeleteReducer,
-  promocodeList: promocodeListReducer,
-  promocodeCreate: promocodeCreateReducer,
-  promocodeById: promocodeByIdReducer,
-  promocodeEdit: promocodeEditReducer,
-  promocodeDelete: promocodeDeleteReducer,
-  reviews: reviewReducer,
-  addReview: addReviewReducer,
-  contactsEdit: contactsEditReducer,
-  adminProductSearch: adminProductSearchReducer,
-  adminReviewsSearch: adminReviewsSearchReducer,
-  usersList: usersListReducer,
-  userDelete: userDeleteReducer,
-  userCreate: userCreateReducer,
-  userEdit: userEditReducer,
-  userById: userByIdReducer,
-});
+// Function to create the combined reducer
+function createReducer(asyncReducers = {}) {
+  return combineReducers({
+    ...staticReducers,
+    ...asyncReducers,
+  });
+}
 
+// Configure the store with only static reducers initially
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: createReducer(),
   middleware: (getDefaultMiddleware) => [...getDefaultMiddleware()],
   devTools: 'process.env.NODE_ENV' !== 'production',
   preloadedState,
 });
 
+// Attach async reducers dictionary to the store instance
+store.asyncReducers = {};
+
+/**
+ * Injects multiple reducers at once and replaces the store's root reducer.
+ * @param {Object} reducersMap An object where keys are the state path and values are the reducer functions.
+ */
+export const injectReducers = (reducersMap) => {
+  let newReducersInjected = false;
+
+  // Add new reducers to the asyncReducers dictionary
+  Object.entries(reducersMap).forEach(([key, reducer]) => {
+    if (reducer && !store.asyncReducers[key]) {
+      store.asyncReducers[key] = reducer;
+      newReducersInjected = true;
+    }
+  });
+
+  if (newReducersInjected) {
+    // Replace the reducer once after all new reducers are added
+    store.replaceReducer(createReducer(store.asyncReducers));
+    console.log(`Injected ${Object.keys(reducersMap).length} async reducers.`);
+  }
+};
+
+// Store subscription logic to save state to local storage
 store.subscribe(() => {
   const { cart, auth, viewedProducts } = store.getState();
   saveToStorage('cart', cart);
