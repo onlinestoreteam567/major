12d810180232de0715a557ef141d25a73062c8c6
Fresh Lemur <dimalimaredmi@gmail.com>
2025-10-22 14:34:08 +0300

    feature: add loading spinner for contacts in Footer components to improve web core vitals

--- client/src/components/Footer/FooterTopSection/FooterInfo/FooterInfo.jsx
+++ client/src/components/Footer/FooterTopSection/FooterInfo/FooterInfo.jsx
@@ -1,35 +1,41 @@
+import Spinner from '@components/UI/Spinner/Spinner';
 import cl from './index.module.scss';
 import useTranslationNamespace from '@hooks/useTranslationNamespace';
 
 const FooterInfo = ({ contacts }) => {
   const { getTranslation } = useTranslationNamespace('footer');
 
-  if (!contacts) return;
   return (
     <section className={cl.footerInfo}>
-      <div className={`${cl.workingHoursWrapper} ${cl.wrapper}`}>
-        <ul className={cl.workingHours}>
-          <li className={cl.liWithImg}>
-            <img src="/svg/footer/calendar.svg" alt={getTranslation('calendarAlt')} />
-            <h3>{contacts[0].work_schedule_weekdays}</h3>
-          </li>
-          <li>
-            <h3>{contacts[0].work_schedule_weekends}</h3>
-          </li>
-        </ul>
-      </div>
+      {!contacts ? (
+        <Spinner />
+      ) : (
+        <>
+          <div className={`${cl.workingHoursWrapper} ${cl.wrapper}`}>
+            <ul className={cl.workingHours}>
+              <li className={cl.liWithImg}>
+                <img src="/svg/footer/calendar.svg" alt={getTranslation('calendarAlt')} />
+                <h3>{contacts[0].work_schedule_weekdays}</h3>
+              </li>
+              <li>
+                <h3>{contacts[0].work_schedule_weekends}</h3>
+              </li>
+            </ul>
+          </div>
 
-      <div className={`${cl.phoneNumberWrapper} ${cl.wrapper}`}>
-        <ul>
-          <li className={cl.liWithImg}>
-            <img className={cl.phoneCallIcon} src="/svg/footer/phoneCall.svg" alt={getTranslation('phoneAlt')} />
-            <h3 className={cl.titleWithIcon}>{contacts[0].main_phone_number}</h3>
-          </li>
-          <li>
-            <h3>{contacts[0].additional_phone_number}</h3>
-          </li>
-        </ul>
-      </div>
+          <div className={`${cl.phoneNumberWrapper} ${cl.wrapper}`}>
+            <ul>
+              <li className={cl.liWithImg}>
+                <img className={cl.phoneCallIcon} src="/svg/footer/phoneCall.svg" alt={getTranslation('phoneAlt')} />
+                <h3 className={cl.titleWithIcon}>{contacts[0].main_phone_number}</h3>
+              </li>
+              <li>
+                <h3>{contacts[0].additional_phone_number}</h3>
+              </li>
+            </ul>
+          </div>
+        </>
+      )}
     </section>
   );
 };
--- client/src/components/Footer/FooterTopSection/FooterNavigation/FooterNavigation.jsx
+++ client/src/components/Footer/FooterTopSection/FooterNavigation/FooterNavigation.jsx
@@ -1,32 +1,36 @@
 import cl from './index.module.scss';
 import useTranslationNamespace from '@hooks/useTranslationNamespace';
 import NavLink from './NavLink/NavLink';
+import Spinner from '@components/UI/Spinner/Spinner';
 
 const FooterNavigation = ({ contacts }) => {
   const { getTranslation } = useTranslationNamespace('footer');
-  if (!contacts) return;
 
   return (
     <div className={cl.footerNavigation}>
-      <div className={cl.wrapList}>
-        <ul className={cl.wrapLinks}>
-          <NavLink type="footer" to="cooperation" ariaLabel={getTranslation('ariaLabelCooperation')}>
-            {getTranslation('cooperation')}
-          </NavLink>
-          <li>
-            <a href={contacts[0].offer_agreement_policy}>{getTranslation('publicOffer')}</a>
-          </li>
-        </ul>
-        <ul className={cl.wrapLinks}>
-          <li>
-            <a href={contacts[0].exchange_and_return_policy}>{getTranslation('exchangeAndReturn')}</a>
-          </li>
+      {!contacts ? (
+        <Spinner />
+      ) : (
+        <div className={cl.wrapList}>
+          <ul className={cl.wrapLinks}>
+            <NavLink type="footer" to="cooperation" ariaLabel={getTranslation('ariaLabelCooperation')}>
+              {getTranslation('cooperation')}
+            </NavLink>
+            <li>
+              <a href={contacts[0].offer_agreement_policy}>{getTranslation('publicOffer')}</a>
+            </li>
+          </ul>
+          <ul className={cl.wrapLinks}>
+            <li>
+              <a href={contacts[0].exchange_and_return_policy}>{getTranslation('exchangeAndReturn')}</a>
+            </li>
 
-          <li>
-            <a href={contacts[0].paymant_and_delivery_policy}>{getTranslation('paymentAndDelivery')}</a>
-          </li>
-        </ul>
-      </div>
+            <li>
+              <a href={contacts[0].paymant_and_delivery_policy}>{getTranslation('paymentAndDelivery')}</a>
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
