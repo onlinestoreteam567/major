ed9c9c82ac7e54d4dd2ffc478408d67763024ffa
Stanislav <ssovelich@gmail.com>
2025-11-01 11:53:19 +0200

    fix animation

--- client/src/pages/HomePage/OurPartners/Map/PartnerInfo/PartnerInfo.jsx
+++ client/src/pages/HomePage/OurPartners/Map/PartnerInfo/PartnerInfo.jsx
@@ -1,26 +1,54 @@
 import cl from './index.module.scss';
 import useTranslationNamespace from '@hooks/useTranslationNamespace';
 import Heading from '@UI/Texts/Heading/Heading';
-import { handleCloseWithDelay } from '@utils/handleCloseWithDelay';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const PartnerInfo = ({ informationAboutPartner, setInformationAboutPartner }) => {
-  const [isHiddenAnimation, setIsHiddenAnimation] = useState();
+  const [isVisible, setIsVisible] = useState(false);
+  const [isClosing, setIsClosing] = useState(false);
   const { getTranslation } = useTranslationNamespace('ourPartners');
-  const handleClose = () => handleCloseWithDelay(setIsHiddenAnimation, setInformationAboutPartner);
+
+  useEffect(() => {
+    if (informationAboutPartner) {
+      setIsVisible(true);
+      setIsClosing(false);
+    }
+  }, [informationAboutPartner]);
+
+  const handleClose = () => {
+    if (isClosing) return;
+    setIsClosing(true);
+    setTimeout(() => {
+      setIsVisible(false);
+      setIsClosing(false);
+      setInformationAboutPartner(null);
+    }, 500);
+  };
+
+  if (!informationAboutPartner) return null;
 
   return (
-    <aside className={`${cl.partnerInfo} ${isHiddenAnimation ? cl.hiddenAnimation : ''}`} onMouseLeave={handleClose}>
+    <aside
+      className={`${cl.partnerInfo} ${isVisible ? cl.visible : ''} ${isClosing ? cl.hiddenAnimation : ''}`}
+      onMouseLeave={handleClose}
+    >
       <Heading type="h2">{informationAboutPartner.name}</Heading>
+
       <section className={cl.workScheduleSection}>
         <Heading type="h3">{getTranslation('workingHours')}:</Heading>
-        <Heading type="h4">{getTranslation('workDays')}: {informationAboutPartner.work_schedule_weekdays}</Heading>
-        <Heading type="h4">{getTranslation('weekendDays')}: {informationAboutPartner.work_schedule_weekends}</Heading>
+        <Heading type="h4">
+          {getTranslation('workDays')}: {informationAboutPartner.work_schedule_weekdays}
+        </Heading>
+        <Heading type="h4">
+          {getTranslation('weekendDays')}: {informationAboutPartner.work_schedule_weekends}
+        </Heading>
       </section>
+
       <section className={cl.addressSection}>
         <Heading type="h3">{getTranslation('address')}:</Heading>
         <Heading type="h4">{informationAboutPartner.addres}</Heading>
       </section>
+
       <a
         className={cl.googleMapLink}
         href={informationAboutPartner.google_maps_link}
