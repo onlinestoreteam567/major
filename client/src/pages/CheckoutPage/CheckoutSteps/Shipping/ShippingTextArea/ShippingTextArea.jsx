5c9267793bf5f884d756f4f5080470ab93549c0f
Lemur <dimalimaredmi@gmail.com>
2025-11-24 08:18:15 +0200

    fix: adjust textArea width to 100% for deskmax

--- client/src/pages/CheckoutPage/CheckoutSteps/Shipping/ShippingTextArea/ShippingTextArea.jsx
+++ client/src/pages/CheckoutPage/CheckoutSteps/Shipping/ShippingTextArea/ShippingTextArea.jsx
@@ -2,14 +2,12 @@ import cl from './index.module.scss';
 import Paragraph from '@components/UI/Texts/Paragraph/Paragraph';
 import useTranslationNamespace from '@hooks/useTranslationNamespace';
 
-export function ShippingTextArea({ labelText, placeholder, name, register, errors, ...rest }) {
+export function ShippingTextArea({ placeholder, name, register, errors, ...rest }) {
   const { getTranslation } = useTranslationNamespace('checkoutPage');
 
   return (
     <>
       <label className={`${cl.shippingTextArea} ${errors && errors[name] ? cl.error : ''}`} htmlFor={name}>
-        <Paragraph type="body2">{labelText}</Paragraph>
-
         <textarea placeholder={placeholder} {...register(name)} {...rest} id={name} />
         {errors?.[name] && <Paragraph type="caption">{getTranslation(errors[name].message)}</Paragraph>}
       </label>
