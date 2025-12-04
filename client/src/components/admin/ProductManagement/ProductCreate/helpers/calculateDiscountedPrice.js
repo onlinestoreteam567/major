8d8108ccd6827837fae50b3de6c39369a1ce3c69
Lemur <dimalimaredmi@gmail.com>
2025-12-04 08:34:23 +0200

    fix: update rounding method for discounted price calculation

--- client/src/components/admin/ProductManagement/ProductCreate/helpers/calculateDiscountedPrice.js
+++ client/src/components/admin/ProductManagement/ProductCreate/helpers/calculateDiscountedPrice.js
@@ -1,7 +1,7 @@
 const calculateDiscountedPrice = (price, discount, setValue) => {
   if (price && discount) {
     const discounted = price - (price * discount) / 100;
-    setValue('discounted_price', Math.round(discounted));
+    setValue('discounted_price', Math.ceil(discounted));
   } else if (price) {
     setValue('discounted_price', price);
   } else {
